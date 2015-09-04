02a3640bf020d5c18f1caf8991788dc4fd26bab6
Philip Crotwell <crotwell@seis.sc.edu>
2015-09-04 11:58:40 -0400

    for use by broccoli-dist-es6-module

---
+++ Brocfile.js
@@ -0,0 +1,19 @@
+var dist = require('broccoli-dist-es6-module');
+
+module.exports = dist('src', {
+
+  // the entry script, and module that becomes the global
+  main: 'seisplot',
+
+  // will become window.ic.ajax with the exports from `main`
+  global: 'seisplot',
+
+  // the prefix for named-amd modules
+  packageName: 'seisplot',
+
+  // global output only: naive shimming, when the id 'ember' is imported,
+  // substitute with `window.Ember` instead
+  shim: {
+    'ember': 'Ember'
+  }
+});
