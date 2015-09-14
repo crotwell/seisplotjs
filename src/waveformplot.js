018fd38ff02e41b0a6a2673bb1c1e031ea6e3a1b
Philip Crotwell <crotwell@seis.sc.edu>
2015-09-14 16:29:03 -0400

    refactor waveformplot as es6 class instead of function

--- src/waveformplot.js
+++ src/waveformplot.js
@@ -11,114 +11,116 @@ import miniseed from './miniseed';
 import seedcodec from './seedcodec';
 import d3 from 'd3';
 
-export default function() {
  
-    console.log("In waveformplot");
-    var plotStart;
-    var plotEnd;
+class chart {
+    constructor(inSvgParent, inSegments) {
+        console.log("In waveformplot");
+        this.throttleResize = true;
+        this.plotStart;
+        this.plotEnd;
+        
+        // d3 margin convention, see http://bl.ocks.org/mbostock/3019563
+        this.margin = {top: 20, right: 20, bottom: 40, left: 75};
+        this.outerWidth = 960;
+        this.outerHeight = 500;
+        this.width = this.outerWidth - this.margin.left - this.margin.right;
+        this.height = this.outerHeight - this.margin.top - this.margin.bottom;
+        // d3 margin convention, see http://bl.ocks.org/mbostock/3019563
     
-    // d3 margin convention, see http://bl.ocks.org/mbostock/3019563
-    var margin = {top: 20, right: 20, bottom: 40, left: 75};
-    var outerWidth = 960;
-    var outerHeight = 500;
-    var width = outerWidth - margin.left - margin.right;
-    var height = outerHeight - margin.top - margin.bottom;
-    // d3 margin convention, see http://bl.ocks.org/mbostock/3019563
-    
-    var segments = [];
-    var svgParent;
-    var xScale;
-    var yScale;
-    var xAxis;
-    var yAxis;
-    var xLabel = "Time";
-    var xSublabel = "";
-    var yLabel = "Amplitude";
-    var ySublabel = "";
-    var plotUUID = guid();
-    var clipPathId = "clippath_"+plotUUID;
-
-    var chart = function(inSvgParent, inSegments) {
+        this.segments = [];
+        this.svgParent;
+        this.xScale;
+        this.yScale;
+        this.xAxis;
+        this.yAxis;
+        this.xLabel = "Time";
+        this.xSublabel = "";
+        this.yLabel = "Amplitude";
+        this.ySublabel = "";
+        this.plotUUID = guid();
+        this.clipPathId = "clippath_"+plotUUID;
         segments.push(inSegments);
-        svgParent = inSvgParent;
+        this.svgParent = inSvgParent;
         draw();
         
       //  fix this....
       //  addResizeHandler(resize);
     }
+
     
-    chart.append = function(key, segment) {
-        segments.push(segment);
+    append(key, segment) {
+        this.segments.push(segment);
     }
     
-    function resize() {
+    resize() {
         /*
          * This only works if added to the window, see addResizeHandler in crocusplot.js
          */
 
-        var svg = svgParent.select("svg");
-        var svgG = svg.select("g");
+        let svgP = this.svgParent;
+        let svg = this.svgP.select("svg");
+        let svgG = svg.select("g");
         /* Find the new window dimensions */
-        var targetWidth = svgParent[0][0].clientWidth;
-        var targetHeight = svgParent[0][0].clientWidth;
+        let targetWidth = this.svgP[0][0].clientWidth;
+        let targetHeight = this.svgP[0][0].clientHeight;
         console.log("target parent: "+targetWidth+"  "+targetHeight);
         
-        var styleWidth = parseInt(svgParent.style("width")) ;
-        var styleHeight = parseInt(svgParent.style("height")) ;
+        let styleWidth = parseInt(svgP.style("width")) ;
+        let styleHeight = parseInt(svgP.style("height")) ;
         if (styleHeight == 0) { styleHeight = 100;}
         console.log("style parent: "+styleWidth+"  "+styleHeight);
         setWidthHeight(svg, styleWidth, styleHeight);
         console.log("resize "+width+" "+height);
 
         /* Update the range of the scale with new width/height */
-        xScale.range([0, width]);
-        yScale.range([height/segments.length, 0]);
+        this.xScale.range([0, this.width]);
+        this.yScale.range([this.height/this.segments.length, 0]);
         
         /* Update the axis with the new scale */
         svgG.select('.x.axis')
-          .attr("transform",  "translate(0," + (height ) + " )")
+          .attr("transform",  "translate(0," + (this.height ) + " )")
           .call(xAxis);
 
         svgG.selectAll('.y.axis')
           .call(yAxis);
 
         svg.select('g.xLabel')
-            .attr("transform", "translate("+(margin.left+(width)/2)+", "+(outerHeight  - 6)+")");
+            .attr("transform", "translate("+(this.margin.left+(this.width)/2)+", "+(this.outerHeight  - 6)+")");
             
             
         svg.select('g.yLabel')
-            .attr("transform", "translate(0, "+(margin.top+(height)/2)+")");
+            .attr("transform", "translate(0, "+(this.margin.top+(this.height)/2)+")");
             
 
-        svg.select('#'+clipPathId).select("rect")
-              .attr("width", width)
-              .attr("height", height);
+        svg.select('#'+this.clipPathId).select("rect")
+              .attr("width", this.width)
+              .attr("height", this.height);
         
         svgG.select("rect.graphClickPane")
-              .attr("width", width)
-              .attr("height", height);
+              .attr("width", this.width)
+              .attr("height", this.height);
         
         /* Force D3 to recalculate and update the line segments*/
-        for (var plotNum=0; plotNum < segments.length; plotNum++) {
-            for (var drNum = 0; drNum < segments[plotNum].length; drNum++) { 
-                console.log("resize select: "+'#'+segments[plotNum][drNum].seisId()+'_'+plotUUID);
-                svgG.select('#'+segments[plotNum][drNum].seisId()+'_'+plotUUID)
+        for (let plotNum=0; plotNum < this.segments.length; plotNum++) {
+            for (let drNum = 0; drNum < this.segments[plotNum].length; drNum++) { 
+                console.log("resize select: "+'#'+this.segments[plotNum][drNum].seisId()+'_'+this.plotUUID);
+                svgG.select('#'+segments[plotNum][drNum].seisId()+'_'+this.plotUUID)
                 .attr("d", createLineFunction(segments[plotNum][drNum]));
         }
         }
     }
 
-    chart.xScale = function() {
-        return xScale;
-    };
-    chart.yScale = function() {
-        return yScale;
+    getXScale() {
+        return this.xScale;
     }
-    chart.getResizeFunction = function() {
-        return resize;
+    getYScale() {
+        return this.yScale;
+    }
+    getResizeFunction() {
+        return this.resize;
     }
     
-    function createLineFunction(segment) {
+    createLineFunction(segment) {
         this.seg = segment;
         return d3.svg.line()
         .x(function(d, i) {
@@ -128,28 +130,28 @@ export default function() {
         }).interpolate("linear")(seg); // call the d3 function created by line with data
     }
     
-    function draw() {
-        console.log("In waveformplot.draw "+plots.length+" "+width+" "+height);
-        var sampPeriod = 1;
-        var minAmp = 2 << 24;
-        var maxAmp = -1 * (minAmp);
-        var count = 0;
-        var s;
-        var e;
-        var record;
-        var n;
-        var connectingDR;
-        if (plots.length > 0) {
-            if(!plotStart) {
-               plotStart = segments[0][0].start;
+    draw() {
+        console.log("In waveformplot.draw "+this.plots.length+" "+this.width+" "+this.height);
+        let sampPeriod = 1;
+        let minAmp = 2 << 24;
+        let maxAmp = -1 * (minAmp);
+        let count = 0;
+        let s;
+        let e;
+        let record;
+        let n;
+        let connectingDR;
+        if (this.plots.length > 0) {
+            if(!this.plotStart) {
+               this.plotStart = segments[0][0].start;
             }
-            if(!plotEnd) {
-                plotEnd = segments[0][0].end;
+            if(!this.plotEnd) {
+                this.plotEnd = segments[0][0].end;
             }
         }
-        for (var plotNum=0; plotNum < segments.length; plotNum++) {
-            for (var drNum = 0; drNum < segments.length; drNum++) {
-                record = segments[plotNum][drNum];
+        for (let plotNum=0; plotNum < this.segments.length; plotNum++) {
+            for (let drNum = 0; drNum < this.segments.length; drNum++) {
+                record = this.segments[plotNum][drNum];
                 s = record.start;
                 e = record.end;
                 console.log("segment "+plotNum+" " + drNum + " " + s + "  " + e);
@@ -161,55 +163,55 @@ export default function() {
                         maxAmp = record[n];
                     }
                 }
-                if (s < plotStart) {
-                    plotStart = s;
+                if (s < this.plotStart) {
+                    this.plotStart = s;
                 }
-                if (plotEnd < e) {
-                    plotEnd = e;
+                if (this.plotEnd < e) {
+                    this.plotEnd = e;
                 }
             }
         }
-        outerWidth = parseInt(svgParent.style("width")) ;
-        outerHeight = parseInt(svgParent.style("height")) ;
-        var svg = svgParent.append("svg");
+        this.outerWidth = parseInt(svgParent.style("width")) ;
+        this.outerHeight = parseInt(svgParent.style("height")) ;
+        let svg = svgParent.append("svg");
         setWidthHeight(svg, outerWidth, outerHeight);
 
-        var svgG = svg
+        let svgG = svg
             .append("g")
-            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+            .attr("transform", "translate(" + this.margin.left + "," + this.margin.top + ")");
         
 
-        svgG.append("defs").append("clipPath").attr("id", clipPathId)
+        svgG.append("defs").append("clipPath").attr("id", this.clipPathId)
             .append("rect")
-              .attr("width", width)
-              .attr("height", height);
-        xScale = d3.time.scale().domain([ plotStart, plotEnd ])
-            .range([ 0, width ])
+              .attr("width", this.width)
+              .attr("height", this.height);
+        xScale = d3.time.scale().domain([ this.plotStart, this.plotEnd ])
+            .range([ 0, this.width ])
             .nice();
         yScale = d3.scale.linear().domain([ minAmp, maxAmp ])
-            .range([ height/segments/length, 0 ])
+            .range([ this.height/this.segments/length, 0 ])
             .nice();
-        xAxis = d3.svg.axis().scale(xScale).orient("bottom").ticks(5);
+        xAxis = d3.svg.axis().scale(this.xScale).orient("bottom").ticks(5);
 
-        yAxis = d3.svg.axis().scale(yScale).orient("left").ticks(5);
+        yAxis = d3.svg.axis().scale(this.yScale).orient("left").ticks(5);
         
         svgG.append("g").attr("class", "x axis")
-            .attr("transform",  "translate(0," + (height ) + " )")
-            .call(xAxis);
-        svgG.append("g").attr("class", "y axis").call(yAxis);
+            .attr("transform",  "translate(0," + (this.height ) + " )")
+            .call(this.xAxis);
+        svgG.append("g").attr("class", "y axis").call(this.yAxis);
         
         
-        var seisG = svgG.selectAll("g").data(segments).enter().append("g").attr("id", function(d) {return d[0].seisId();});
-        var seisPath = seisG.selectAll("path").data(function(d) {return d;})
+        let seisG = svgG.selectAll("g").data(this.segments).enter().append("g").attr("id", function(d) {return d[0].seisId();});
+        let seisPath = seisG.selectAll("path").data(function(d) {return d;})
             .enter().append("path")
             .classed("seispath")
-            .attr("id", function(d) {return d.seisId()+'_'+plotUUID})
+            .attr("id", function(d) {return d.seisId()+'_'+this.plotUUID})
             .attr("d", function(d) {return createLineFunction(d)});
         
         /*
-        var seismogram = svgG.append("g").attr("class", "seismogram").attr("clip-path", "url(#"+clipPathId+")");
+        let seismogram = svgG.append("g").attr("class", "seismogram").attr("clip-path", "url(#"+clipPathId+")");
         
-        var seisLine = seismogram.selectAll("path").data(segments, function(d) {return d.seisId();});
+        let seisLine = seismogram.selectAll("path").data(segments, function(d) {return d.seisId();});
         seisLine.enter().append("path")
             .attr("id", function(d) {return d.seisId()+'_'+plotUUID})
             .attr("d", function(d) {return createLineFunction(d)});
@@ -217,14 +219,14 @@ export default function() {
 */
         svg.append("g")
             .attr("class", "xLabel")
-            .attr("transform", "translate("+(margin.left+(width)/2)+", "+(outerHeight  - 6)+")")
+            .attr("transform", "translate("+(this.margin.left+(this.width)/2)+", "+(this.outerHeight  - 6)+")")
             .append("text").attr("class", "x label")
             .attr("text-anchor", "middle")
-            .text(xLabel);
+            .text(this.xLabel);
         svg.append("g")
             .attr("class", "yLabel")
             .attr("x", 0)
-            .attr("transform", "translate(0, "+(margin.top+(height)/2)+")")
+            .attr("transform", "translate(0, "+(this.margin.top+(this.height)/2)+")")
            .append("text")
             .attr("class", "y label")
             .attr("text-anchor", "middle")
@@ -233,114 +235,115 @@ export default function() {
             .attr("transform", "rotate(-90)")
             .text(yLabel);
         svgG.append("rect").attr("class", "graphClickPane")
-            .attr("width", width)
-            .attr("height", height);
+            .attr("width", this.width)
+            .attr("height", this.height);
         resize();
     }
     
     
 
 
-    function setWidthHeight(svg, nOuterWidth, nOuterHeight) {
-        outerWidth = nOuterWidth;
-        outerHeight = nOuterHeight;
-        width = outerWidth - margin.left - margin.right;
-        height = outerHeight - margin.top - margin.bottom;
+    setWidthHeight(svg, nOuterWidth, nOuterHeight) {
+        this.outerWidth = nOuterWidth;
+        this.outerHeight = nOuterHeight;
+        this.width = outerWidth - this.margin.left - this.margin.right;
+        this.height = outerHeight - this.margin.top - this.margin.bottom;
         svg
-          .attr("width", outerWidth)
-          .attr("height", outerHeight);
-    };
+          .attr("width", this.outerWidth)
+          .attr("height", this.outerHeight);
+    }
     
     /*
      * from http://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid-in-javascript
      */
-    function s4() {
+    s4() {
           return Math.floor((1 + Math.random()) * 0x10000)
                      .toString(16)
                      .substring(1);
-    };
-    function guid() {
+    }
+    guid() {
           return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
                  s4() + '-' + s4() + s4() + s4();
-    };
+    }
     
 
     // see http://blog.kevinchisholm.com/javascript/javascript-function-throttling/
-    var throttleResize = true;
-    var throttle = function(func, delay){
+    throttle(func, delay){
         if (throttleResize) {
             window.clearTimeout(throttleResize);
         }
         throttleResize = window.setTimeout(func, delay);
-    };
+    }
     
-    chart.resizeNeeded = function() {
+    resizeNeeded() {
         throttle(function(){
             resize();
         }, 250);
-    };
+    }
 
-    chart.plotStart = function(value) {
+    set plotStart(value) {
         if (!arguments.length)
-            return plotStart;
-        plotStart = value;
-        xScale.domain([ plotStart, plotEnd ])
-        chart.resizeNeeded();
-        return chart;
-    };
-    chart.plotEnd = function(value) {
+            return this.plotStart;
+        this.plotStart = value;
+        this.xScale.domain([ this.plotStart, this.plotEnd ])
+        resizeNeeded();
+        return this;
+    }
+    set plotEnd(value) {
         if (!arguments.length)
-            return plotEnd;
-        plotEnd = value;
-        xScale.domain([ plotStart, plotEnd ])
-        chart.resizeNeeded();
-        return chart;
-    };
+            return this.plotEnd;
+        this.plotEnd = value;
+        this.xScale.domain([ this.plotStart, this.plotEnd ])
+        resizeNeeded();
+        return this;
+    }
     
-    chart.width = function(value) {
+    set width(value) {
         if (!arguments.length)
-            return width;
-        width = value;
-        return chart;
-    };
-
-    chart.height = function(value) {
-        if (!arguments.length)
-            return height;
-        height = value;
-        return chart;
-    };
-
-    chart.margin = function(value) {
-        if (!arguments.length)
-            return margin;
-        margin = value;
-        return chart;
-    }
-    chart.xLabel = function(value) {
-        if (!arguments.length)
-            return xLabel;
-        xLabel = value;
-        return chart;
-    }
-    chart.yLabel = function(value) {
-        if (!arguments.length)
-            return yLabel;
-        yLabel = value;
-        return chart;
-    }
-    chart.xSublabel = function(value) {
-        if (!arguments.length)
-            return xSublabel;
-        xSublabel = value;
-        return chart;
-    }
-    chart.ySublabel = function(value) {
-        if (!arguments.length)
-            return ySublabel;
-        ySublabel = value;
-        return chart;
+            return this.width;
+        this.width = value;
+        return this;
     }
 
+    set height(value) {
+        if (!arguments.length)
+            return this.height;
+        this.height = value;
+        return this;
+    }
+
+    set margin(value) {
+        if (!arguments.length)
+            return this.margin;
+        this.margin = value;
+        return this;
+    }
+    set xLabel(value) {
+        if (!arguments.length)
+            return this.xLabel;
+        this.xLabel = value;
+        return this;
+    }
+    set yLabel(value) {
+        if (!arguments.length)
+            return this.yLabel;
+        this.yLabel = value;
+        return this;
+    }
+    set xSublabel(value) {
+        if (!arguments.length)
+            return this.xSublabel;
+        this.xSublabel = value;
+        return this;
+    }
+    set ySublabel(value) {
+        if (!arguments.length)
+            return this.ySublabel;
+        this.ySublabel = value;
+        return this;
+    }
 }
+
+
+export { chart }
 
