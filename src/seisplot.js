8a40acf4b5d4ab9b54052f8d72b37809764832c6
Philip Crotwell <crotwell@seis.sc.edu>
2015-09-15 13:34:04 -0400

    clean up redundant import, just use re-exported

--- src/seisplot.js
+++ src/seisplot.js
@@ -4,9 +4,12 @@
  * http://www.seis.sc.edu
  */
 
-import miniseed from './miniseed.js';
-import seedcodec from './seedcodec.js';
+//import miniseed from './miniseed.js';
+//import seedcodec from './seedcodec.js';
 import waveformplot from './waveformplot.js';
+
+let miniseed = waveformplot.miniseed;
+let seedcodec = waveformplot.seedcodec;
 
 export { miniseed, seedcodec, waveformplot};
 
