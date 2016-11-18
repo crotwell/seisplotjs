613f5c5ea17ff6d4fa765199e44fa9d6123ad471
Philip Crotwell <crotwell@seis.sc.edu>
2016-11-18 10:05:37 -0500

    change into a umbrella package to contain the subpackages

--- makeDist.sh
+++
@@ -1,5 +0,0 @@
-
-rm -r dist
-mkdir dist
-cat src/miniseed.js src/seedcodec.js src/waveformplot.js src/seisplot.js | babel --out-file dist/seisplot.js
-cp package.json dist/.
---
+++ src/index.js
@@ -0,0 +1,15 @@
+/**
+ * Philip Crotwell
+ * University of South Carolina, 2016
+ * http://www.seis.sc.edu
+ */
+
+import * as miniseed from 'seisplotjs-miniseed';
+import * as seedcodec from 'seisplotjs-seedcodec';
+import * as seedlink from 'seisplotjs-seedlink';
+import * as waveformplot from 'seisplotjs-waveformplot';
+
+/* reexport */
+export { seedcodec, miniseed, seedlink, waveformplot };
+
+
