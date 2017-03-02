50ce520911d764829d35dfc51b4e7acfcf609299
Philip Crotwell <crotwell@seis.sc.edu>
2017-03-02 12:47:42 -0500

    add more subpackages

--- src/index.js
+++ src/index.js
@@ -5,12 +5,22 @@
  */
 
 import * as distaz from 'seisplotjs-distaz';
+import * as fdsnevent from 'seisplotjs-fdsnevent';
+import * as fdsnstation from 'seisplotjs-fdsnstation';
 import * as miniseed from 'seisplotjs-miniseed';
 import * as seedcodec from 'seisplotjs-seedcodec';
 import * as seedlink from 'seisplotjs-seedlink';
+import * as traveltime from 'seisplotjs-traveltime';
 import * as waveformplot from 'seisplotjs-waveformplot';
 
 /* reexport */
-export { distaz, seedcodec, miniseed, seedlink, waveformplot };
+export { distaz, 
+         fdsnevent,
+         fdsnstation,
+         seedcodec, 
+         miniseed, 
+         seedlink, 
+         traveltime, 
+         waveformplot };
 
 
