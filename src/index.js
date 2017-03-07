037c8a337b78ed558ae5067cb1cdc46efa9bc34b
Philip Crotwell <crotwell@seis.sc.edu>
2017-03-07 16:45:39 -0500

    add fdsndataselect, upgrade eslint

--- src/index.js
+++ src/index.js
@@ -7,20 +7,24 @@
 import * as distaz from 'seisplotjs-distaz';
 import * as fdsnevent from 'seisplotjs-fdsnevent';
 import * as fdsnstation from 'seisplotjs-fdsnstation';
+import * as fdsndataselect from 'seisplotjs-fdsndataselect';
 import * as miniseed from 'seisplotjs-miniseed';
 import * as seedcodec from 'seisplotjs-seedcodec';
 import * as seedlink from 'seisplotjs-seedlink';
 import * as traveltime from 'seisplotjs-traveltime';
 import * as waveformplot from 'seisplotjs-waveformplot';
+let d3 = waveformplot.d3;
 
 /* reexport */
 export { distaz, 
          fdsnevent,
          fdsnstation,
+         fdsndataselect,
          seedcodec, 
          miniseed, 
          seedlink, 
          traveltime, 
-         waveformplot };
+         waveformplot,
+         d3 };
 
 
