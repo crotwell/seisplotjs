993306a01f94d333448541e2a32c590d5ca88c53
Philip Crotwell <crotwell@seis.sc.edu>
2017-03-13 15:00:12 -0400

    add more tests (bestGuessEventId, publicId, dataSelectRecent). Clean up lint errors

--- example/serviceCheck/allServiceTests.js
+++ example/serviceCheck/allServiceTests.js
@@ -26,7 +26,6 @@ var testEventVersion = {
   testname: "Ev Ver",
   testid: "eventversion",
   test: function(dc) {
-    var mythis = this;
     var host = serviceHost(dc, EV);
 
     var quakeQuery = new fdsnevent.EventQuery()
@@ -39,7 +38,7 @@ var testEventVersion = {
         url: url
       };
     }).catch(function(err) {
-      if (! err.url) {err.url = url};
+      if (! err.url) {err.url = url;}
       throw err;
     });
   }
@@ -50,7 +49,6 @@ var testStationVersion = {
   testname: "Sta Ver",
   testid: "stationversion",
   test: function(dc) {
-    var mythis = this;
     var host = serviceHost(dc, ST);
 
     var query = new fdsnstation.StationQuery()
@@ -63,7 +61,7 @@ var testStationVersion = {
         url: url
       };
     }).catch(function(err) {
-      if (! err.url) {err.url = url};
+      if (! err.url) {err.url = url;}
       throw err;
     });
   }
@@ -73,7 +71,6 @@ var testDataSelectVersion = {
   testname: "DS Ver",
   testid: "dataselectversion",
   test: function(dc) {
-    var mythis = this;
     var host = serviceHost(dc, ST);
 
     var query = new fdsndataselect.DataSelectQuery()
@@ -86,7 +83,7 @@ var testDataSelectVersion = {
         url: url
       };
     }).catch(function(err) {
-      if (! err.url) {err.url = url};
+      if (! err.url) {err.url = url;}
       throw err;
     });
   }
@@ -98,7 +95,6 @@ var testLastDay = {
   testid: "lastday",
   test: function(dc) {
     return new RSVP.Promise(function(resolve, reject) {
-    var mythis = this;
     if ( ! doesSupport(dc, EV) ) {
       reject(UNSUPPORTED);
     } else {
@@ -114,12 +110,94 @@ var testLastDay = {
     var url = quakeQuery.formURL();
     return quakeQuery.query().then(function(quakes) {
       return {
-        text: "Found "+quakes.length+" events.",
+        text: "Found "+quakes.length,
         url: url,
         output: quakes
       };
     }).catch(function(err) {
-      if (! err.url) {err.url = url};
+      if (! err.url) {err.url = url;}
+      throw err;
+    });
+    });
+  }
+};
+
+
+
+var testEventFromPublicID = {
+  testname: "eventid=publicID",
+  testid: "eventid_publicid",
+  test: function(dc) {
+    return new RSVP.Promise(function(resolve, reject) {
+      if ( ! doesSupport(dc, EV) ) {
+        reject(UNSUPPORTED);
+      } else {
+        resolve(null);
+      }
+   }).then(function(val) {
+    var daysAgo = .5;
+    var host = serviceHost(dc, EV);
+    var quakeQuery = new fdsnevent.EventQuery()
+      .host(host)
+      .startTime(new Date(new Date().getTime()-86400*daysAgo*1000))
+      .endTime(new Date());
+    var url = quakeQuery.formURL();
+    return quakeQuery.query().then(function(quakes) {
+        if (quakes.length == 0) {
+          throw new Error("No quakes returned");
+        }
+        var singleQuakeQuery = new fdsnevent.EventQuery()
+          .host(host)
+          .eventid(encodeURIComponent(quakes[0].publicID));
+        return singleQuakeQuery.query();
+      }).then(function(singleQuake) {
+      return {
+        text: "Found "+singleQuake.time(),
+        url: url,
+        output: singleQuake
+      };
+    }).catch(function(err) {
+      if (! err.url) {err.url = url;}
+      throw err;
+    });
+    });
+  }
+};
+
+var testEventFromBestGuessEventId = {
+  testname: "Best Guess EventId",
+  testid: "guesseventid",
+  test: function(dc) {
+    return new RSVP.Promise(function(resolve, reject) {
+      if ( ! doesSupport(dc, EV) ) {
+        reject(UNSUPPORTED);
+      } else {
+        resolve(null);
+      }
+   }).then(function(val) {
+    var daysAgo = .5;
+    var host = serviceHost(dc, EV);
+    var quakeQuery = new fdsnevent.EventQuery()
+      .host(host)
+      .startTime(new Date(new Date().getTime()-86400*daysAgo*1000))
+      .endTime(new Date());
+    var url = quakeQuery.formURL();
+    return quakeQuery.query().then(function(quakes) {
+        if (quakes.length == 0) {
+          throw new Error("No quakes returned");
+        }
+        var singleQuakeQuery = new fdsnevent.EventQuery()
+          .host(host)
+          .eventid(encodeURIComponent(quakes[0].eventid));
+        return singleQuakeQuery.query();
+      }).then(function(quakes) {
+      return {
+        text: "Found "+quakes.length,
+        url: url,
+        output: quakes
+      };
+    }).catch(function(err) {
+      if (! err.url) {err.url = url;}
       throw err;
     });
     });
@@ -131,7 +209,6 @@ var testCatalogs = {
   testid: "catalogs",
   test: function(dc) {
     return new RSVP.Promise(function(resolve, reject) {
-    var mythis = this;
     if ( ! doesSupport(dc, EV) ) {
       reject(UNSUPPORTED);
     } else {
@@ -144,12 +221,12 @@ var testCatalogs = {
     var url = quakeQuery.formCatalogsURL();
     return quakeQuery.queryCatalogs().then(function(catalogs) {
       return {
-        text: "Found "+catalogs.length+" catalogs.",
+        text: "Found "+catalogs.length,
         url: url,
         output: catalogs
       };
     }).catch(function(err) {
-      if (! err.url) {err.url = url};
+      if (! err.url) {err.url = url;}
       throw err;
     });
     });
@@ -161,7 +238,6 @@ var testContributors = {
   testid: "contributors",
   test: function(dc) {
     return new RSVP.Promise(function(resolve, reject) {
-    var mythis = this;
     if ( ! doesSupport(dc, EV) ) {
       reject(UNSUPPORTED);
     } else {
@@ -174,12 +250,12 @@ var testContributors = {
     var url = quakeQuery.formContributorsURL();
     return quakeQuery.queryContributors().then(function(contributors) {
       return {
-        text: "Found "+contributors.length+" contributors.",
+        text: "Found "+contributors.length,
         url: url,
         output: contributors
       };
     }).catch(function(err) {
-      if (! err.url) {err.url = url};
+      if (! err.url) {err.url = url;}
       throw err;
     });
     });
@@ -191,7 +267,6 @@ var testNetworks = {
   testid: "networks",
   test: function(dc) {
     return new RSVP.Promise(function(resolve, reject) {
-    var mythis = this;
     if ( ! doesSupport(dc, ST) ) {
       reject(UNSUPPORTED);
     } else {
@@ -207,18 +282,75 @@ var testNetworks = {
     var url = query.formURL(fdsnstation.LEVEL_NETWORK);
     return query.queryNetworks().then(function(networks) {
       return {
-        text: "Found "+networks.length+" networks.",
+        text: "Found "+networks.length,
         url: url,
         output: networks
       };
     }).catch(function(err) {
-      if (! err.url) {err.url = url};
+      if (! err.url) {err.url = url;}
       throw err;
     });
     });
   }
 };
 
+
+var testDataSelectRecent = {
+  testname: "Recent Data",
+  testid: "recentData",
+  test: function(dc) {
+    return new RSVP.Promise(function(resolve, reject) {
+    if ( ! doesSupport(dc, DS) || ! doesSupport(dc, ST) ) {
+      reject(UNSUPPORTED);
+    } else {
+      resolve(null);
+    }
+   }).then(function(val) {
+    var host = serviceHost(dc, ST);
+    var query = new fdsnstation.StationQuery()
+      .host(host)
+      .channelCode("BHZ");
+//      .endAfter(new Date())
+//      .matchTimeseries(true);
+    return query.queryNetworks().then(function(nets) {
+      if (nets.length == 0) {
+        throw new Error("No Networks returned from "+query.formURL(fdsnstation.LEVEL_NETWORK));
+      }
+      query.networkCode(nets[0].networkCode())
+        .channelCode("BHZ");
+//        .endAfter(new Date())
+//        .matchTimeseries(true);
+      return query.queryStations();
+     });
+   }).then(function(networks) {
+    if (networks.length == 0 || networks[0].stations.length == 0 ) {
+      throw new Error("No Stations returned from "+query.formURL(fdsnstation.LEVEL_STATION));
+    }
+    return networks[0].stations()[0];
+   }).then(function(station) {
+    var host = serviceHost(dc, DS);
+
+    var query = new fdsndataselect.DataSelectQuery()
+      .host(host);
+    var url = query
+      .networkCode(station.network().networkCode())
+      .stationCode(station.stationCode())
+      .channelCode("BHZ")
+      .computeStartEnd(null, new Date(), 300, 0)
+      .formURL(fdsnstation.LEVEL_NETWORK);
+    return query.query().then(function(miniseed) {
+      return {
+        text: "Found "+miniseed.length,
+        url: url,
+        output: miniseed
+      };
+    }).catch(function(err) {
+      if (! err.url) {err.url = url;}
+      throw err;
+    });
+    });
+  }
+};
 // end test defs
 
 var RUN = "Run";
@@ -243,10 +375,29 @@ function serviceHost(dc, type) {
 }
 
 var tests = {
-     fdsnEventTests: [ testEventVersion, testLastDay, testCatalogs, testContributors ],
+     fdsnEventTests: [ testEventVersion, testLastDay, testCatalogs, testContributors, testEventFromBestGuessEventId, testEventFromPublicID ],
      fdsnStationTests: [ testStationVersion, testNetworks ],
-     fdsnDataTests: [ testDataSelectVersion ]
- }
+     fdsnDataTests: [ testDataSelectVersion, testDataSelectRecent ]
+ };
 
+var notVersionTest = {
+     fdsnEventTests: [  testLastDay, testCatalogs, testContributors, testEventFromBestGuessEventId, testEventFromPublicID ],
+     fdsnStationTests: [  testNetworks ],
+     fdsnDataTests: [ testDataSelectRecent ]
+ };
+var justOneTest = {
+     fdsnEventTests: [ ],
+     fdsnStationTests: [ ],
+     fdsnDataTests: [ testDataSelectRecent]
+};
+var justVersionTest = {
+     fdsnEventTests: [ testEventVersion ],
+     fdsnStationTests: [ testStationVersion ],
+     fdsnDataTests: [ testDataSelectVersion ]
+};
+
+//return notVersionTest;
+//return justVersionTest;
+//return justOneTest;
 return tests;
 }();
