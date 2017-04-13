206a9fbd98ddabee6c964ac51ad178bb6f1c99b1
Philip Crotwell <crotwell@seis.sc.edu>
2017-04-13 17:04:41 -0400

    lint as es5 for examples instead of es6 as no transpiling

---
+++ .eslintrc_examples.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "extends": "eslint:recommended",
+    "installedESLint": true,
+    "env": {
+      "browser": true
+    },
+    "parserOptions": {
+      "ecmaVersion": 5
+    },
+    "rules": {
+      "semi": ["error", "always"]
+    }
+};
