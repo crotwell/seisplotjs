3d5b7251d7478331102da1bb80905e432ed7a900
Philip Crotwell <crotwell@seis.sc.edu>
2017-08-10 09:11:35 -0400

    formatting, model was not exported

--- src/index.js
+++ src/index.js
@@ -4,16 +4,17 @@
  * http://www.seis.sc.edu
  */
 
-import * as distaz from 'seisplotjs-distaz';
-import * as fdsnevent from 'seisplotjs-fdsnevent';
-import * as fdsnstation from 'seisplotjs-fdsnstation';
+import * as distaz         from 'seisplotjs-distaz';
+import * as fdsnevent      from 'seisplotjs-fdsnevent';
+import * as fdsnstation    from 'seisplotjs-fdsnstation';
 import * as fdsndataselect from 'seisplotjs-fdsndataselect';
-import * as filter from 'seisplotjs-filter';
-import * as miniseed from 'seisplotjs-miniseed';
-import * as seedcodec from 'seisplotjs-seedcodec';
-import * as seedlink from 'seisplotjs-seedlink';
-import * as traveltime from 'seisplotjs-traveltime';
-import * as waveformplot from 'seisplotjs-waveformplot';
+import * as filter         from 'seisplotjs-filter';
+import * as miniseed       from 'seisplotjs-miniseed';
+import * as model          from 'seisplotjs-model';
+import * as seedcodec      from 'seisplotjs-seedcodec';
+import * as seedlink       from 'seisplotjs-seedlink';
+import * as traveltime     from 'seisplotjs-traveltime';
+import * as waveformplot   from 'seisplotjs-waveformplot';
 let d3 = waveformplot.d3;
 
 /* reexport */
@@ -22,8 +23,9 @@ export { distaz,
          fdsnstation,
          fdsndataselect,
          filter,
-         seedcodec, 
          miniseed, 
+         model,
+         seedcodec, 
          seedlink, 
          traveltime, 
          waveformplot,
