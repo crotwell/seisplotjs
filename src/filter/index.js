ec0a49aba862ff9d37decfc17c1f0452034bae29
Philip Crotwell <crotwell@seis.sc.edu>
2019-02-04 14:44:57 -0500

    rmean and minmax functions

--- src/filter/index.js
+++ src/filter/index.js
@@ -25,18 +25,92 @@ export function amplitude(real: number, imag: number) {
   return Math.hypot(real, imag);
 }
 
-export function rMean(seis: model.Seismogram) :model.Seismogram {
-  let out = seis.clone();
-  let meanVal = mean(seis);
-  let demeanY = seis.y.map(function(d) {
-    return d-meanVal;
-  });
-  out.y = demeanY;
-  return out;
+export function rMean(seis: model.Seismogram|model.Trace) :model.Seismogram {
+  console.log(`rMean input class is: ${(seis.constructor.name)}`)
+  if (seis instanceof model.Trace) {
+    let meanVal = 0;
+    let npts = seis.numPoints;
+    for (let s of seis.seisArray) {
+      meanVal += mean(s)*s.numPoints;
+    }
+    meanVal = meanVal / npts;
+    let rmeanTrace = new model.Trace(seis.seisArray.map(s =>{
+        let demeanY = s.y.map(function(d) {
+          return d-meanVal;
+        });
+        let out = s.clone();
+        out.y = demeanY;
+        return out;
+      }));
+    return rmeanTrace;
+  } else if (seis instanceof model.Seismogram) {
+    let out = seis.clone();
+    let meanVal = mean(seis);
+    let demeanY = seis.y.map(function(d) {
+      return d-meanVal;
+    });
+    out.y = demeanY;
+    return out;
+  } else {
+    throw new Error("rMean arg not a Seismogram or Trace");
+  }
 }
 
-export function mean(waveform: model.Seismogram): number {
-  return meanOfSlice(waveform.y, waveform.y.length);
+export function gainCorrect(instrumentSensitivity : model.InstrumentSensitivity, seis: model.Seismogram|model.Trace) {
+  if (seis instanceof model.Trace) {
+    let gainTrace = new model.Trace(seis.seisArray.map(s =>{
+      return gainCorrect(instrumentSensitivity, s);
+      }));
+    return gainTrace;
+  } else {
+      let out = seis.clone();
+      let gain = instrumentSensitivity.sensitivity;
+      let gainY = seis.y.map(function(d) {
+        return d/gain;
+      });
+      out.y = gainY;
+      out.yUnit = instrumentSensitivity.inputUnit;
+      return out;
+  }
+}
+
+
+export function minMaxMean(seis: model.Seismogram|model.Trace): number {
+  let meanVal = 0;
+  let minVal = 9999999999;
+  let maxVal = -9999999999;
+  if (seis instanceof model.Trace) {
+    let npts = seis.numPoints;
+    for (let s of seis.seisArray) {
+      meanVal += mean(s)*s.numPoints;
+      minVal = s.y.reduce((acc, val) => {return Math.min(acc, val);}, minVal);
+      maxVal = s.y.reduce((acc, val) => {return Math.max(acc, val);}, maxVal);
+    }
+    meanVal = meanVal / npts;
+  } else {
+    meanVal += mean(seis);
+    minVal = seis.y.reduce((acc, val) => {return Math.min(acc, val);}, minVal);
+    maxVal = seis.y.reduce((acc, val) => {return Math.max(acc, val);}, maxVal);
+  }
+  return {
+    min: minVal,
+    max: maxVal,
+    mean: meanVal
+  };
+}
+export function mean(waveform: model.Seismogram|model.Trace): number {
+  if (waveform instanceof model.Trace) {
+    let meanVal = 0;
+
+    let npts = waveform.numPoints;
+    for (let s of waveform.seisArray) {
+      meanVal += mean(s)*s.numPoints;
+    }
+    meanVal = meanVal / npts;
+    return meanVal;
+  } else {
+    return meanOfSlice(waveform.y, waveform.y.length);
+  }
 }
 
 function meanOfSlice(dataSlice: Array<number>, totalPts :number ):number {
