8a9bf7967d2499b713850af092457f37537ff282
Philip Crotwell <crotwell@seis.sc.edu>
2019-05-22 17:23:44 -0400

    local crc-32c method, bug fixes from testing

--- test/xseed/xseed_file.test.js
+++ test/xseed/xseed_file.test.js
@@ -1,3 +1,5 @@
+var TextDecoder = require('util').TextDecoder;
+
 import * as xseed from '../../src/xseed.js';
 import  {moment} from '../../src/util';
 
@@ -15,13 +17,40 @@ let fileList = [
   'test/xseed/reference-data/reference-sinusoid-int32.xseed',
   'test/xseed/reference-data/reference-sinusoid-steim1.xseed',
   'test/xseed/reference-data/reference-sinusoid-steim2.xseed'];
+
+let fileSizeMap = new Map();
+fileSizeMap.set('test/xseed/reference-data/reference-ascii.xseed',295);
+fileSizeMap.set('test/xseed/reference-data/reference-detectiononly.xseed',331);
+fileSizeMap.set('test/xseed/reference-data/reference-sinusoid-FDSN-All.xseed',3787);
+fileSizeMap.set('test/xseed/reference-data/reference-sinusoid-FDSN-Other.xseed',1149);
+fileSizeMap.set('test/xseed/reference-data/reference-sinusoid-TQ-TC-ED.xseed',1318);
+fileSizeMap.set('test/xseed/reference-data/reference-sinusoid-float32.xseed',2060);
+fileSizeMap.set('test/xseed/reference-data/reference-sinusoid-float64.xseed',4060);
+fileSizeMap.set('test/xseed/reference-data/reference-sinusoid-int16.xseed',1060);
+fileSizeMap.set('test/xseed/reference-data/reference-sinusoid-int32.xseed',2060);
+fileSizeMap.set('test/xseed/reference-data/reference-sinusoid-steim1.xseed',956);
+fileSizeMap.set('test/xseed/reference-data/reference-sinusoid-steim2.xseed',956);
+
+  fileList = fileList.slice(2,3);
+
 for (let filename of fileList) {
   test("ref xseed file vs json"+filename, () => {
     console.log("test xseed "+filename);
     expect(fs.existsSync(filename)).toEqual(true);
     let xData = fs.readFileSync(filename);
-    //expect(xData.length).toEqual(2060);
-    console.log("test xseed "+xData.slice(0,2));
+    expect(xData.length).toEqual(fileSizeMap.get(filename));
+    let hexStr = "";
+    for (let i=0;i<xData.length; i++) {
+      let s = "";
+      if (i >= xseed.CRC_OFFSET && i < xseed.CRC_OFFSET+4) {
+        s = "00";
+      } else {
+        s = xData[i].toString(16).toUpperCase();
+        if (s.length === 1) { s = "0"+s;}
+      }
+      hexStr += s;
+    }
+    console.log(`record as hex (${hexStr.length}): ${hexStr}`);
     expect(xData[0]).toEqual(77);
     expect(xData[1]).toEqual(83);
     expect(xData[2]).toEqual(3);
@@ -34,6 +63,7 @@ for (let filename of fileList) {
     expect(parsed.length).toEqual(1);
     let xr = parsed[0];
     let xh = xr.header;
+    expect(xr.getSize()).toEqual(fileSizeMap.get(filename));
 
     let jsonFilename = filename.slice(0, -5)+'json';
 
@@ -51,7 +81,11 @@ for (let filename of fileList) {
     expect(xh.encoding).toEqual(jsonData.EncodingFormat);
     expect(xh.sampleRatePeriod).toEqual(jsonData.SampleRate);
     expect(xh.numSamples).toEqual(jsonData.SampleCount);
-    expect('0x'+xh.crc.toString(16).toUpperCase()).toEqual(jsonData.CRC);
+    expect(xseed.crcToHexString(xh.crc)).toEqual(jsonData.CRC);
+    let crc = xr.calcCrc();
+    // doesn't work as json is not identical after round trip
+    // due to / being same as \/, also 1e-6 and .000001
+    //expect(xseed.crcToHexString(crc)).toEqual(jsonData.CRC);
     expect(xh.publicationVersion).toEqual(jsonData.PublicationVersion);
     expect(xh.extraHeadersLength).toEqual(jsonData.ExtraLength);
     if (xh.extraHeadersLength > 2) {
@@ -60,7 +94,12 @@ for (let filename of fileList) {
   });
 }
 
-
-test("ms2 round trip", () => {
-
-});
+test("crc-32c of a string", () => {
+  let s = "hi12345";
+  let buf = new Uint8Array(new ArrayBuffer(s.length));
+  for (let i=0; i<s.length; i++) {
+    buf[i] = s.charCodeAt(i)
+  }
+  let crc = xseed.calculateCRC32C(buf);
+  console.log("crc="+'0x'+crc.toString(16).toUpperCase())
+})
