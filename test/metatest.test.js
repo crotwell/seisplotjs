b4612b49b1ab2734f14d7876bbfeafe63eff6ccc
Philip Crotwell <crotwell@seis.sc.edu>
2019-06-12 13:23:04 -0400

    flow fixes

--- test/metatest.test.js
+++ test/metatest.test.js
@@ -1,7 +1,10 @@
 // @flow
 
 test("metatest", () => {
+  // $FlowFixMe
   expect(.0001).toBeCloseToRatio(0, 3, .0002, 3);
+    // $FlowFixMe
   expect(7.001).toBeCloseToRatio(7, 3);
+    // $FlowFixMe
   expect([7001, 1999]).arrayToBeCloseToRatio([7000, 2000], 3);
 });
