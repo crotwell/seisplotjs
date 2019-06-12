19eca3a0e7b3f56be2639b031248ce606399b77e
Philip Crotwell <crotwell@seis.sc.edu>
2019-06-12 11:18:43 -0400

    add parse to create SacPoleZero from string

--- src/sacPoleZero.js
+++ src/sacPoleZero.js
@@ -49,4 +49,70 @@ export class SacPoleZero {
     s.push("CONSTANT "+this.constant);
     return s.join('\n');
   }
+  /** Parses a string in sac polezero format into a SacPoleZero.
+  */
+  static parse(data: string) {
+      let pz = {
+        zeros: [],
+        poles: [],
+        constant: 1
+      };
+      let lines = data.split('\n');
+      let numZeros = 0;
+      let numPoles = 0;
+      let i=0;
+      while (i < lines.length) {
+        let l = lines[i];
+        let items = l.trim().split(/ +/);
+        if (items[0] === 'ZEROS') {
+          numZeros = parseInt(items[1]);
+          i++;
+          l = lines[i];
+          items = l.trim().split(/ +/);
+          while (i < lines.length && pz.zeros.length < numZeros) {
+            if (items[0] === 'POLES') {
+              // no more zeros, fill array with 0
+              for(let z = pz.zeros.length; z < numZeros; z++) {
+                pz.zeros.push(filter.createComplex(0,0));
+              }
+              break;
+            } else {
+              let real = parseFloat(items[0]);
+              let imag = parseFloat(items[1]);
+              pz.zeros.push(filter.createComplex(real, imag));
+            }
+            i++;
+            l = lines[i];
+            items = l.trim().split(/ +/);
+          }
+        }
+        if (items[0] === 'POLES') {
+          numPoles = parseInt(items[1]);
+          i++;
+          l = lines[i];
+          items = l.trim().split(/ +/);
+          while (i < lines.length && pz.poles.length < numPoles) {
+            if (items[0] === 'CONSTANT') {
+              // no more poles, fill array with 0
+              for(let z = pz.poles.length; z < numPoles; z++) {
+                pz.poles.push(filter.createComplex(0,0));
+              }
+              break;
+            } else {
+              let real = parseFloat(items[0]);
+              let imag = parseFloat(items[1]);
+              pz.poles.push(filter.createComplex(real, imag));
+            }
+            i++;
+            l = lines[i];
+            items = l.trim().split(/ +/);
+          }
+        }
+        if (items[0] === 'CONSTANT') {
+          pz.constant = parseFloat(items[1]);
+        }
+        i++;
+      }
+      return new SacPoleZero(pz.poles, pz.zeros, pz.constant);
+  }
 }
