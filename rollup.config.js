17829038546ef83d8272dc52130aace4768e8024
Philip Crotwell <crotwell@seis.sc.edu>
2019-07-02 15:16:57 -0400

    seemingly ok rollup building of dist js as umd and esm

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,49 @@
-import * as meta from "./package.json";
+import * as pkg from "./package.json";
 import babel from 'rollup-plugin-babel';
+import flowEntry from 'rollup-plugin-flow-entry';
 
 export default [{
   input: 'src/index.js',
   output: [
     {
-      file: `dist/seisplot-${meta.version}-umd.js`,
+      file: pkg.main,
       name: "seisplotjs",
-      format: 'umd'
-    },
-    {
-      file: `dist/seisplot-${meta.version}-esm.js`,
-      format: 'esm'
+      format: 'umd',
+      sourcemap: true,
     }
   ],
+  external: [
+    ...Object.keys(pkg.dependencies || {}),
+    ...Object.keys(pkg.peerDependencies || {}),
+  ],
   plugins: [
+    flowEntry(),
     babel({
       exclude: 'node_modules/**',
-      runtimeHelpers: true
+      runtimeHelpers: true,
     })
   ]
-}];
+},{
+  input: 'src/index.js',
+  preserveModules: true,
+  output: [
+    {
+      dir: "dist/module",
+      //file: pkg.module,
+      format: 'esm',
+      sourcemap: true,
+    }
+  ],
+  external: [
+    ...Object.keys(pkg.dependencies || {}),
+    ...Object.keys(pkg.peerDependencies || {}),
+  ],
+  plugins: [
+    flowEntry(),
+    babel({
+      exclude: 'node_modules/**',
+      runtimeHelpers: true,
+    })
+  ]
+  }
+];
