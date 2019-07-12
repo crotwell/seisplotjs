303b6f1ab30ab0c60cf92893c8fec3f4f1158668
Philip Crotwell <crotwell@seis.sc.edu>
2019-07-12 12:42:19 -0400

    make flow happier

--- src/waveformplot/inject_css_manual.js
+++ src/waveformplot/inject_css_manual.js
@@ -5,6 +5,7 @@
 
 export function insertCSS(cssText: string) {
   let head = document.head;
+  if (head === null) {throw new Error("document.head is null");}
   let styleElement = document.createElement('style');
   styleElement.type = 'text/css';
   styleElement.appendChild(document.createTextNode(cssText));
