8d6c24947b016df908ea0f949107851fc83c47b3
Philip Crotwell <crotwell@seis.sc.edu>
2019-07-12 11:19:22 -0400

    remove filter subdir

--- src/filter/hilbert.js
+++
@@ -1,6 +0,0 @@
-//@flow
-
-import * as OregonDSPTop from 'oregondsp';
-const OregonDSP = OregonDSPTop.com.oregondsp.signalProcessing;
-const CenteredHilbertTransform = OregonDSP.filter.fir.equiripple.CenteredHilbertTransform;
-import {Seismogram } from '../seismogram';
