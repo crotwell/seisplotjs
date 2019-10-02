a862ffb6e040de9b32416ce1df462c093780ae67
Philip Crotwell <crotwell@seis.sc.edu>
2019-10-02 13:01:48 -0400

    flow and jsdoc, return the inserted element

--- src/cssutil.js
+++ src/cssutil.js
@@ -6,11 +6,18 @@
  * http://www.seis.sc.edu
  */
 
-export function insertCSS(cssText: string) {
+/**
+ * Inserts text as css into the head of an html document. No checking
+ * as to validity of the css is done, just inserts a style
+ * element at the beginning of the head.
+ * @param  cssText textual css for insertion
+ */
+export function insertCSS(cssText: string): HTMLElement {
   let head = document.head;
   if (head === null) {throw new Error("document.head is null");}
   let styleElement = document.createElement('style');
   styleElement.type = 'text/css';
   styleElement.appendChild(document.createTextNode(cssText));
   head.insertBefore(styleElement, head.firstChild);
+  return styleElement;
 }
