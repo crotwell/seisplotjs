1cc672698abac395f45e069c9d9c186ea01d77af
Philip Crotwell <crotwell@seis.sc.edu>
2019-12-11 15:38:45 -0500

    remove commented out log, error on default case in switch

--- src/seedcodec.js
+++ src/seedcodec.js
@@ -352,13 +352,11 @@ export function decodeSteim2(dataView: DataView, numSamples: number, swapBytes: 
     tempSamples = extractSteim2Samples(dataView, i*64, swapBytes);   // returns only differences except for frame 0
     firstData = 0; // d(0) is byte 0 by default
     if (i===0) {   // special case for first frame
-      // console.log("i===0, special case for first frame")
       lastValue = bias; // assign our X(-1)
       // x0 and xn are in 1 and 2 spots
       start = tempSamples[1];  // X(0) is byte 1 for frame 0
       // end = tempSamples[2];    // X(n) is byte 2 for frame 0
       firstData = 3; // d(0) is byte 3 for frame 0
-      //console.log("DEBUG: frame " + i + ", bias = " + bias + ", x(0) = " + start + ", x(n) = " + end);
       // if bias was zero, then we want the first sample to be X(0) constant
       if (bias === 0) lastValue = start - tempSamples[3];  // X(-1) = X(0) - d(0)
     }
@@ -366,7 +364,6 @@ export function decodeSteim2(dataView: DataView, numSamples: number, swapBytes: 
     for (let j = firstData; j < tempSamples.length && current < numSamples; j++) {
       samples[current] = lastValue + tempSamples[j];  // X(n) = X(n-1) + d(n)
       lastValue = samples[current];
-      //console.log("d(" + (j-firstData) + ")" + tempSamples[j] + ", x(" + current + ")" + samples[current] + ";");
       current++;
     }
     //System.err.println("DEBUG: end of frame " + i);
@@ -410,14 +407,14 @@ function extractSteim2Samples(dataView: DataView, offset: number, swapBytes: boo
     currNibble = (nibbles >> (30 - i*2 ) ) & 0x03;
     switch (currNibble) {
       case 0:
-        //System.out.println("0 means header info");
+        // "0 means header info"
         // only include header info if offset is 0
         if (offset === 0) {
           temp[currNum++] = dataView.getInt32(offset+(i*4), swapBytes);
         }
         break;
       case 1:
-        //console.log("1 means 4 one byte differences " +currNum+" "+dataView.getInt8(offset+(i*4))+" "+dataView.getInt8(offset+(i*4)+1)+" "+dataView.getInt8(offset+(i*4)+2)+" "+dataView.getInt8(offset+(i*4)+3));
+        //  "1 means 4 one byte differences " +currNum+" "+dataView.getInt8(offset+(i*4))+" "+dataView.getInt8(offset+(i*4)+1)+" "+dataView.getInt8(offset+(i*4)+2)+" "+dataView.getInt8(offset+(i*4)+3)
         temp[currNum++] = dataView.getInt8(offset+(i*4));
         temp[currNum++] = dataView.getInt8(offset+(i*4)+1);
         temp[currNum++] = dataView.getInt8(offset+(i*4)+2);
@@ -428,59 +425,62 @@ function extractSteim2Samples(dataView: DataView, offset: number, swapBytes: boo
         dnib = (tempInt >> 30) & 0x03;
         switch (dnib) {
           case 1:
-            //console.log("2,1 means 1 thirty bit difference");
+            // "2,1 means 1 thirty bit difference"
             temp[currNum++] = (tempInt << 2) >> 2;
             break;
           case 2:
-            //console.log("2,2 means 2 fifteen bit differences");
+            // "2,2 means 2 fifteen bit differences"
             temp[currNum++] = (tempInt << 2) >> 17;  // d0
             temp[currNum++] = (tempInt << 17) >> 17; // d1
             break;
           case 3:
-            //console.log("2,3 means 3 ten bit differences");
+            //  "2,3 means 3 ten bit differences"
             temp[currNum++] = (tempInt << 2) >> 22;  // d0
             temp[currNum++] = (tempInt << 12) >> 22; // d1
             temp[currNum++] = (tempInt << 22) >> 22; // d2
             break;
           default:
-            //console.log("default");
-      }
-      break;
-    case 3:
-      tempInt = dataView.getUint32(offset+(i*4), swapBytes);
-      dnib = (tempInt >> 30) & 0x03;
-      // for case 3, we are going to use a for-loop formulation that
-      // accomplishes the same thing as case 2, just less verbose.
-      diffCount = 0;  // number of differences
-      bitSize = 0;    // bit size
-      headerSize = 0; // number of header/unused bits at top
-      switch (dnib) {
-        case 0:
-          //System.out.println("3,0 means 5 six bit differences");
-          headerSize = 2;
-          diffCount = 5;
-          bitSize = 6;
-          break;
-        case 1:
-          //System.out.println("3,1 means 6 five bit differences");
-          headerSize = 2;
-          diffCount = 6;
-          bitSize = 5;
-          break;
-        case 2:
-          //System.out.println("3,2 means 7 four bit differences, with 2 unused bits");
-          headerSize = 4;
-          diffCount = 7;
-          bitSize = 4;
-          break;
-        default:
-          //System.out.println("default");
-      }
-      if (diffCount > 0) {
-        for (let d=0; d<diffCount; d++) {  // for-loop formulation
-          temp[currNum++] = ( tempInt << (headerSize+(d*bitSize)) ) >> (((diffCount-1)*bitSize) + headerSize);
+            throw new CodecException(`Unknown case currNibble=${currNibble} dnib=${dnib}`);
         }
-      }
+        break;
+      case 3:
+        tempInt = dataView.getUint32(offset+(i*4), swapBytes);
+        dnib = (tempInt >> 30) & 0x03;
+        // for case 3, we are going to use a for-loop formulation that
+        // accomplishes the same thing as case 2, just less verbose.
+        diffCount = 0;  // number of differences
+        bitSize = 0;    // bit size
+        headerSize = 0; // number of header/unused bits at top
+        switch (dnib) {
+          case 0:
+            //System.out.println("3,0 means 5 six bit differences");
+            headerSize = 2;
+            diffCount = 5;
+            bitSize = 6;
+            break;
+          case 1:
+            //System.out.println("3,1 means 6 five bit differences");
+            headerSize = 2;
+            diffCount = 6;
+            bitSize = 5;
+            break;
+          case 2:
+            //System.out.println("3,2 means 7 four bit differences, with 2 unused bits");
+            headerSize = 4;
+            diffCount = 7;
+            bitSize = 4;
+            break;
+          default:
+            throw new CodecException(`Unknown case currNibble=${currNibble} dnib=${dnib}`);
+        }
+        if (diffCount > 0) {
+          for (let d=0; d<diffCount; d++) {  // for-loop formulation
+            temp[currNum++] = ( tempInt << (headerSize+(d*bitSize)) ) >> (((diffCount-1)*bitSize) + headerSize);
+          }
+        }
+        break;
+      default:
+        throw new CodecException(`Unknown case currNibble=${currNibble}`);
     }
   }
   return temp.slice(0, currNum);
