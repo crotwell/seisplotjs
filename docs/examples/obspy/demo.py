1976733ad572912263e6244123e14ccf001efb0c
Philip Crotwell <crotwell@seis.sc.edu>
2019-12-13 11:01:07 -0500

    clean up demo, check bad messages

--- docs/examples/obspy/demo.py
+++ docs/examples/obspy/demo.py
@@ -4,24 +4,21 @@ import serveobspy
 from obspy.clients.fdsn import Client
 import obspy
 
-
 serveSeis = serveobspy.ServeObsPy('www')
 serveSeis.serveData()
-
-# fake send message manually
-# serveSeis.wsServer.send_json_message("howdy there")
 
 client = Client("IRIS")
 
 start = obspy.UTCDateTime('2019-10-31T01:11:19')
 end = start + 20*60
-quake = client.get_events(starttime=start - 1*60, endtime=start + 20*60, minmagnitude=5)[0]
-serveSeis.quake=quake
-#iris has crazy event ids
-
 
 st = client.get_waveforms("IU", "SNZO", "00", "BH?", start, start + 20 * 60)
 serveSeis.stream=st
+
+quakes = client.get_events(starttime=start - 1*60, endtime=start + 20*60, minmagnitude=5)
+serveSeis.quake=quakes[0]
+#iris has crazy event ids
+
 
 inventory = client.get_stations(network="IU", station="SNZO",
                                 location="00", channel="BH?",
