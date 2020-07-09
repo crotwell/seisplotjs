32f32ff2843bf676e5f337d28ec2c3a532098cf6
Philip Crotwell <crotwell@seis.sc.edu>
2020-07-09 14:59:06 -0400

    add StartEndDuration constructor tests

--- test/util.test.js
+++ test/util.test.js
@@ -1,8 +1,29 @@
 // @flow
 
 import * as util from '../src/util.js';
+import {toIsoWoZ, StartEndDuration, moment} from '../src/util.js';
 
 test( "_toIsoWoZ test", () => {
   const s = "2018-01-01T12:34:45.000";
-  expect(util.toIsoWoZ(util.moment.utc(s))).toBe(s);
+  expect(toIsoWoZ(util.moment.utc(s))).toBe(s);
+});
+
+test("StartEndDuration", () => {
+  const s = "2018-01-01T12:34:45.000";
+  const e = "2018-01-01T12:44:45.000";
+  const d = moment.duration("PT10M");
+  expect(d.humanize()).toBe("10 minutes");
+  let sed = new StartEndDuration(s,e);
+  expect(sed.duration.humanize()).toBe("10 minutes");
+  sed = new StartEndDuration(s,null,d);
+  expect(sed.duration.humanize()).toBe("10 minutes");
+  sed = new StartEndDuration(null,e,d);
+  expect(sed.duration.humanize()).toBe("10 minutes");
+
+  sed = new StartEndDuration(null, null, d);
+  expect(sed.duration.humanize()).toBe("10 minutes");
+  sed = new StartEndDuration(s,null,null);
+  expect(sed.duration.asYears()).toBeGreaterThan(300);
+  expect(sed.endTime).toEqual(util.WAY_FUTURE);
+
 });
