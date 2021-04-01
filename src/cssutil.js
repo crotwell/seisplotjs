71cdcb152dc87f61cbf695c68489ca5e8751c628
Philip Crotwell <crotwell@seis.sc.edu>
2021-04-01 11:38:00 -0400

    only delete by id if it is a style element inside head

--- src/cssutil.js
+++ src/cssutil.js
@@ -6,6 +6,8 @@
  * http://www.seis.sc.edu
  */
 
+export const AUTO_CLASSED = "autoseisplotjs";
+
 /**
  * Inserts text as css into the head of an html document. No checking
  * as to validity of the css is done, just inserts a style
@@ -19,12 +21,18 @@ export function insertCSS(cssText: string, id: string): HTMLElement {
   let head = document.head;
   if (head === null) {throw new Error("document.head is null");}
   if (id) {
-    let existing = document.getElementById(id);
-    if (existing && existing.parentNode) {existing.parentNode.removeChild(existing);}
+    for (let c of head.children) {
+      // only remove if a <style> element, classed with autoseisplotjs and same id within head
+      if (c.localName === "style" && c.id ===id && c.classList.contains(AUTO_CLASSED)) {
+        c.parentNode.removeChild(c);
+        break;
+      }
+    }
   }
   let styleElement = document.createElement('style');
   if (id) { styleElement.id = id; }
   styleElement.type = 'text/css';
+  styleElement.classList.add(AUTO_CLASSED);
   styleElement.appendChild(document.createTextNode(cssText));
   head.insertBefore(styleElement, head.firstChild);
   return styleElement;
