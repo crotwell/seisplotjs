185fd2e11d0971f9b54853245c33d6014acfcfa6
Philip Crotwell <crotwell@seis.sc.edu>
2021-04-05 09:02:06 -0400

    string and num arg check test

--- test/util.test.js
+++ test/util.test.js
@@ -27,3 +27,24 @@ test("StartEndDuration", () => {
   expect(sed.endTime).toEqual(util.WAY_FUTURE);
 
 });
+
+test("isStringArg isNumArg", () => {
+  const s1 = "asad";
+  const s2 = new String("fdsafdsa");
+  const n1 = 7;
+  const n2 = new Number(8);
+  const o = {h: "w"};
+  expect(util.isStringArg(s1)).toBeTrue();
+  expect(util.isStringArg(s2)).toBeTrue();
+  expect(util.isStringArg(n1)).toBeFalse();
+  expect(util.isStringArg(n2)).toBeFalse();
+  expect(util.isStringArg(null)).toBeFalse();
+  expect(util.isStringArg(o)).toBeFalse();
+
+  expect(util.isNumArg(s1)).toBeFalse();
+  expect(util.isNumArg(s2)).toBeFalse();
+  expect(util.isNumArg(n1)).toBeTrue();
+  expect(util.isNumArg(n2)).toBeTrue();
+  expect(util.isNumArg(null)).toBeFalse();
+  expect(util.isNumArg(o)).toBeFalse();
+});
