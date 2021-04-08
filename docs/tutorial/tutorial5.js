48380f58acb71a1f0c7c9bee9fd4c0dbbbf36455
Philip Crotwell <crotwell@seis.sc.edu>
2021-04-08 17:39:39 -0400

    update to use seismogramloader in tutorial 5

--- docs/tutorial/tutorial5.js
+++ docs/tutorial/tutorial5.js
@@ -1,48 +1,42 @@
+
+// snip start eventandstation
 let queryTimeWindow = new seisplotjs.util.StartEndDuration('2019-07-01', '2019-07-31');
 let eventQuery = new seisplotjs.fdsnevent.EventQuery()
   .timeWindow(queryTimeWindow)
   .minMag(7)
   .latitude(35).longitude(-118)
   .maxRadius(3);
-// snip start station
 let stationQuery = new seisplotjs.fdsnstation.StationQuery()
   .networkCode('CO')
   .stationCode('HODGE')
   .locationCode('00')
   .channelCode('HH?')
   .timeWindow(queryTimeWindow);
-Promise.all( [ eventQuery.query(), stationQuery.queryResponses() ] )
-// snip end
-  .then( ( [ quakeList, networks ] ) => {
-    let taupQuery = new seisplotjs.traveltime.TraveltimeQuery()
-      .latLonFromStation(networks[0].stations[0])
-      .latLonFromQuake(quakeList[0])
-      .phases("P,S");
-    return Promise.all( [ quakeList, networks, taupQuery.queryJson() ] );
-  }).then( ( [ quakeList, networks, ttimes ] ) => {
-    let phaseMarkers = seisplotjs.seismograph.createMarkersForTravelTimes(quakeList[0], ttimes);
-    phaseMarkers.push({
-      markertype: 'predicted',
-      name: "origin",
-      time: seisplotjs.moment.utc(quakeList[0].time)
-    });
-    let allChannels = Array.from(seisplotjs.stationxml.allChannels(networks));
-    let firstP = ttimes.arrivals.find(a => a.phase.startsWith('P'));
-    let startTime = seisplotjs.moment.utc(quakeList[0].time)
-      .add(firstP.time, 'seconds').subtract(30, 'seconds');
-// snip start window
-    let timeWindow = new seisplotjs.util.StartEndDuration(startTime, null, 300);
-// snip end
-    let chanTRList = allChannels.map(c => {
-      let sdd = seisplotjs.seismogram.SeismogramDisplayData.fromChannelAndTimeWindow(c, timeWindow);
-      sdd.addQuake(quakeList);
-      sdd.addMarkers(phaseMarkers);
-      return sdd;
-    });
-    let dsQuery = new seisplotjs.fdsndataselect.DataSelectQuery();
-    return Promise.all( [ quakeList, networks, ttimes, dsQuery.postQuerySeismograms(chanTRList) ] );
+// snip start loaderinit
+let loader = new seisplotjs.seismogramloader.SeismogramLoader(stationQuery, eventQuery);
+loader.startOffset = -30;
+loader.endOffset = 270;
+loader.markedPhaseList = "origin,PP,pP";
+loader.withResponse = true;
+let loaderSDDPromise = loader.loadSeismograms();
+
+// snip start map
+let stationsPromise = loader.networkList.then(networkList => {
+  let staText = "";
+  for (let s of seisplotjs.stationxml.allStations(networkList)) {
+    staText += s.codes();
+  }
+  seisplotjs.d3.select('span#stationCode').text(staText);
+});
+let quakePromise = loader.quakeList.then( quakeList => {
+  let quakeText="";
+  for (const q of quakeList) {
+    quakeText+=quakeList[0].description+" ";
+  }
+  seisplotjs.d3.select('span#earthquakeDescription').text(quakeText);
+});
 // snip start filter
-  }).then( ( [ quakeList, networks, ttimes, seismogramDataList ] ) => {
+loaderSDDPromise.then((seismogramDataList) => {
     seismogramDataList.forEach(sdd => {
       let butterworth = seisplotjs.filter.createButterworth(
                              2, // poles
@@ -58,29 +52,27 @@ Promise.all( [ eventQuery.query(), stationQuery.queryResponses() ] )
           sdd.channel.response, .001, .02, 250, 500);
       sdd.seismogram = correctedSeis;
     });
-    return Promise.all( [ quakeList, networks, ttimes, seismogramDataList ] );
-// snip end
-  }).then( ( [ quakeList, networks, ttimes, seismogramDataList ] ) => {
+    return seismogramDataList ;
+// snip start seisconfig
+  }).then( seismogramDataList  => {
     let div = seisplotjs.d3.select('div#myseismograph');
-    for( let sdd of seismogramDataList) {
-      let seisConfig = new seisplotjs.seismographconfig.SeismographConfig();
-      seisConfig.linkedAmplitudeScale = new seisplotjs.seismograph.LinkedAmpScale();
-      seisConfig.wheelZoom = false;
+    let seisConfig = new seisplotjs.seismographconfig.SeismographConfig();
+    seisConfig.linkedTimeScale = new seisplotjs.seismographconfig.LinkedTimeScale();
+    seisConfig.linkedAmplitudeScale = new seisplotjs.seismograph.LinkedAmpScale();
+    seisConfig.wheelZoom = false;
 // snip start gain
-      seisConfig.doGain = false;
-// snip end
-      seisConfig.doRMean = false;
+    seisConfig.doGain = false;
+    seisConfig.doRMean = false;
+    for( let sdd of seismogramDataList) {
       let subdiv = div.append('div').classed('seismograph', true);
       let graph = new seisplotjs.seismograph.Seismograph(subdiv,
                                                          seisConfig,
                                                          sdd);
       graph.draw();
     }
-    seisplotjs.d3.select('span#stationCode').text(networks[0].stations[0].codes());
-    seisplotjs.d3.select('span#earthquakeDescription').text(quakeList[0].description);
-    return Promise.all( [ quakeList, networks, ttimes, seismogramDataList ] );
+    return seismogramDataList;
 // snip start fft
-  }).then( ( [ quakeList, networks, ttimes, seismogramDataList ] ) => {
+  }).then(seismogramDataList => {
     let div = seisplotjs.d3.select('div#fftplot');
     let fftList = seismogramDataList.map(sdd => {
       if (sdd.seismogram.isContiguous()) {
@@ -92,9 +84,12 @@ Promise.all( [ eventQuery.query(), stationQuery.queryResponses() ] )
     let seisConfig = new seisplotjs.seismographconfig.SeismographConfig();
     let fftPlot = new seisplotjs.fftplot.FFTPlot('div#fftplot', seisConfig, fftList, true);
     fftPlot.draw();
-    return Promise.all( [ quakeList, networks, ttimes, seismogramDataList ] );
-// snip end
-  }).catch( function(error) {
+    return seismogramDataList;
+  });
+
+// snip start cleanup
+Promise.all( [ quakePromise, stationsPromise, loaderSDDPromise ] )
+.catch( function(error) {
     seisplotjs.d3.select("div#myseismograph").append('p').html("Error loading data." +error);
     console.assert(false, error);
   });
