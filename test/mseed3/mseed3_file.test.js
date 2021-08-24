ab50baa47886a27b9a064a693430189f523ce691
Philip Crotwell <crotwell@seis.sc.edu>
2021-08-24 16:09:35 -0400

    rename test, xseed to mseed3

--- test/mseed3/mseed3_file.test.js
+++ test/mseed3/mseed3_file.test.js
@@ -7,40 +7,40 @@ let TextDecoder = require('util').TextDecoder;
 global.TextDecoder = TextDecoder;
 
 import { isDef } from '../../src/util.js';
-import * as xseed from '../../src/xseed.js';
+import * as mseed3 from '../../src/mseed3.js';
 import * as miniseed from '../../src/miniseed.js';
 import fs from 'fs';
 
 let fileList = [
-  'test/xseed/reference-data/reference-ascii.xseed',
-  'test/xseed/reference-data/reference-detectiononly.xseed',
-  'test/xseed/reference-data/reference-sinusoid-FDSN-All.xseed',
-  'test/xseed/reference-data/reference-sinusoid-FDSN-Other.xseed',
-  'test/xseed/reference-data/reference-sinusoid-TQ-TC-ED.xseed',
-  'test/xseed/reference-data/reference-sinusoid-float32.xseed',
-  'test/xseed/reference-data/reference-sinusoid-float64.xseed',
-  'test/xseed/reference-data/reference-sinusoid-int16.xseed',
-  'test/xseed/reference-data/reference-sinusoid-int32.xseed',
-  'test/xseed/reference-data/reference-sinusoid-steim1.xseed',
-  'test/xseed/reference-data/reference-sinusoid-steim2.xseed'];
+  'test/mseed3/reference-data/reference-ascii.xseed',
+  'test/mseed3/reference-data/reference-detectiononly.xseed',
+  'test/mseed3/reference-data/reference-sinusoid-FDSN-All.xseed',
+  'test/mseed3/reference-data/reference-sinusoid-FDSN-Other.xseed',
+  'test/mseed3/reference-data/reference-sinusoid-TQ-TC-ED.xseed',
+  'test/mseed3/reference-data/reference-sinusoid-float32.xseed',
+  'test/mseed3/reference-data/reference-sinusoid-float64.xseed',
+  'test/mseed3/reference-data/reference-sinusoid-int16.xseed',
+  'test/mseed3/reference-data/reference-sinusoid-int32.xseed',
+  'test/mseed3/reference-data/reference-sinusoid-steim1.xseed',
+  'test/mseed3/reference-data/reference-sinusoid-steim2.xseed'];
 
 let fileSizeMap = new Map();
-fileSizeMap.set('test/xseed/reference-data/reference-ascii.xseed',295);
-fileSizeMap.set('test/xseed/reference-data/reference-detectiononly.xseed',331);
-fileSizeMap.set('test/xseed/reference-data/reference-sinusoid-FDSN-All.xseed',3787);
-fileSizeMap.set('test/xseed/reference-data/reference-sinusoid-FDSN-Other.xseed',1149);
-fileSizeMap.set('test/xseed/reference-data/reference-sinusoid-TQ-TC-ED.xseed',1318);
-fileSizeMap.set('test/xseed/reference-data/reference-sinusoid-float32.xseed',2060);
-fileSizeMap.set('test/xseed/reference-data/reference-sinusoid-float64.xseed',4060);
-fileSizeMap.set('test/xseed/reference-data/reference-sinusoid-int16.xseed',1060);
-fileSizeMap.set('test/xseed/reference-data/reference-sinusoid-int32.xseed',2060);
-fileSizeMap.set('test/xseed/reference-data/reference-sinusoid-steim1.xseed',956);
-fileSizeMap.set('test/xseed/reference-data/reference-sinusoid-steim2.xseed',956);
+fileSizeMap.set('test/mseed3/reference-data/reference-ascii.xseed',295);
+fileSizeMap.set('test/mseed3/reference-data/reference-detectiononly.xseed',331);
+fileSizeMap.set('test/mseed3/reference-data/reference-sinusoid-FDSN-All.xseed',3787);
+fileSizeMap.set('test/mseed3/reference-data/reference-sinusoid-FDSN-Other.xseed',1149);
+fileSizeMap.set('test/mseed3/reference-data/reference-sinusoid-TQ-TC-ED.xseed',1318);
+fileSizeMap.set('test/mseed3/reference-data/reference-sinusoid-float32.xseed',2060);
+fileSizeMap.set('test/mseed3/reference-data/reference-sinusoid-float64.xseed',4060);
+fileSizeMap.set('test/mseed3/reference-data/reference-sinusoid-int16.xseed',1060);
+fileSizeMap.set('test/mseed3/reference-data/reference-sinusoid-int32.xseed',2060);
+fileSizeMap.set('test/mseed3/reference-data/reference-sinusoid-steim1.xseed',956);
+fileSizeMap.set('test/mseed3/reference-data/reference-sinusoid-steim2.xseed',956);
 
 //  fileList = fileList.slice(2,3);
 
 for (let filename of fileList) {
-  test("ref xseed file vs json"+filename, () => {
+  test("ref mseed3 file vs json"+filename, () => {
 
     expect(fs.existsSync(filename)).toEqual(true);
     let xData = fs.readFileSync(filename);
@@ -48,7 +48,7 @@ for (let filename of fileList) {
     let hexStr = "";
     for (let i=0;i<xData.length; i++) {
       let s = "";
-      if (i >= xseed.CRC_OFFSET && i < xseed.CRC_OFFSET+4) {
+      if (i >= mseed3.CRC_OFFSET && i < mseed3.CRC_OFFSET+4) {
         s = "00";
       } else {
         s = xData[i].toString(16).toUpperCase();
@@ -65,7 +65,7 @@ for (let filename of fileList) {
     expect(dataView.getUint8(0)).toEqual(77);
     expect(dataView.getUint8(1)).toEqual(83);
     expect(dataView.getUint8(2)).toEqual(3);
-    let parsed = xseed.parseXSeedRecords(ab);
+    let parsed = mseed3.parseMSeed3Records(ab);
     expect(parsed.length).toEqual(1);
     let xr = parsed[0];
     let xh = xr.header;
@@ -91,11 +91,11 @@ for (let filename of fileList) {
     expect(xh.encoding).toEqual(jsonData.EncodingFormat);
     expect(xh.sampleRatePeriod).toEqual(jsonData.SampleRate);
     expect(xh.numSamples).toEqual(jsonData.SampleCount);
-    expect(xseed.crcToHexString(xh.crc)).toEqual(jsonData.CRC);
+    expect(mseed3.crcToHexString(xh.crc)).toEqual(jsonData.CRC);
     // doesn't work as json is not identical after round trip
     // due to / being same as \/, also 1e-6 and .000001
     //let crc = xr.calcCrc();
-    //expect(xseed.crcToHexString(crc)).toEqual(jsonData.CRC);
+    //expect(mseed3.crcToHexString(crc)).toEqual(jsonData.CRC);
     expect(xh.publicationVersion).toEqual(jsonData.PublicationVersion);
 
 
@@ -112,10 +112,10 @@ for (let filename of fileList) {
  * get json ExtraHeaders, but with special case if there are no json headers
  * it returns {} for both {} and the empty string. Makes test case consistent.
  *
- * @param   xr xseed record
+ * @param   xr mseed3 record
  * @returns     json version of extra headers
  */
-function getExtraHeaders(xr: xseed.XSeedRecord) {
+function getExtraHeaders(xr: mseed3.MSeed3Record) {
   const xh = xr.header;
   if (xh.extraHeadersLength > 2) {
     return xr.extraHeaders;
@@ -127,7 +127,7 @@ function getExtraHeaders(xr: xseed.XSeedRecord) {
  * get ExtraHeaders from reference json datafile, but with special case if there are no json headers
  * it returns {} for both {} and undef. Makes test case consistent.
  *
- * @param   jsonData xseed record
+ * @param   jsonData mseed3 record
  * @returns     json version of extra headers
  */
 function getJsonDataExtraHeaders(jsonData: any) {
@@ -143,7 +143,7 @@ test("crc-32c of a string", () => {
   for (let i=0; i<s.length; i++) {
     buf[i] = s.charCodeAt(i);
   }
-  let crc = xseed.calculateCRC32C(buf);
+  let crc = mseed3.calculateCRC32C(buf);
   expect(crc).toEqual(0xe3069283);//0xE3069283
 });
 
@@ -159,7 +159,7 @@ test("text output vs mseed2text", function() {
 "    data payload length: 896 bytes",
 "       payload encoding: STEIM-2 integer compression (val: 11)"
   ];
-  const filename = 'test/xseed/reference-data/reference-sinusoid-steim2.xseed';
+  const filename = 'test/mseed3/reference-data/reference-sinusoid-steim2.xseed';
   expect(fs.existsSync(filename)).toEqual(true);
   let xData = fs.readFileSync(filename);
 
@@ -169,11 +169,11 @@ test("text output vs mseed2text", function() {
   expect(dataView.getUint8(0)).toEqual(77);
   expect(dataView.getUint8(1)).toEqual(83);
   expect(dataView.getUint8(2)).toEqual(3);
-  let parsed = xseed.parseXSeedRecords(ab);
+  let parsed = mseed3.parseMSeed3Records(ab);
   expect(parsed.length).toEqual(1);
   let xr = parsed[0];
   let xh = xr.header;
-  expect(xseed.crcToHexString(readCRC)).toEqual("0x5CFF0548");
+  expect(mseed3.crcToHexString(readCRC)).toEqual("0x5CFF0548");
   expect(readCRC).toEqual(0x5CFF0548);
   expect(readCRC).toEqual(xr.header.crc);
   let xhStr = xh.toString().split('\n');
@@ -185,8 +185,8 @@ test("text output vs mseed2text", function() {
 
 
 test("decomp HODGE", function() {
-  const ms3filename = 'test/xseed/one_record_HODGE_HHZ.ms3';
-  const ms2filename = 'test/xseed/one_record_HODGE_HHZ.ms2';
+  const ms3filename = 'test/mseed3/one_record_HODGE_HHZ.ms3';
+  const ms2filename = 'test/mseed3/one_record_HODGE_HHZ.ms2';
   expect(fs.existsSync(ms3filename)).toEqual(true);
   expect(fs.existsSync(ms2filename)).toEqual(true);
   let xData = fs.readFileSync(ms3filename);
@@ -204,7 +204,7 @@ test("decomp HODGE", function() {
   expect(dataView.getUint8(0)).toEqual(77);
   expect(dataView.getUint8(1)).toEqual(83);
   expect(dataView.getUint8(2)).toEqual(3);
-  let parsed = xseed.parseXSeedRecords(ab);
+  let parsed = mseed3.parseMSeed3Records(ab);
   expect(parsed.length).toEqual(1);
   let xr = parsed[0];
   let xh = xr.header;
