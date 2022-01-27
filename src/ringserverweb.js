59fff006b4ab3ffef6125b4f7604f45ac30c9ab6
Philip Crotwell <crotwell@seis.sc.edu>
2022-01-27 16:56:33 -0500

    flow

--- src/ringserverweb.js
+++ src/ringserverweb.js
@@ -206,13 +206,13 @@ export class RingserverConnection {
       });
   }
 
-  getDataLinkURL() {
+  getDataLinkURL(): string {
     let proto = "ws:";
     if (checkProtocol() === "https:") { proto = "wss:";}
     return proto+'//'+this._host+(this._port===80 ? '' : (':'+this._port))+(this._prefix)+DATALINK_PATH;
   }
 
-  getSeedLinkURL() {
+  getSeedLinkURL(): string {
     let proto = "ws:";
     if (checkProtocol() === "https:") { proto = "wss:";}
     return proto+'//'+this._host+(this._port===80 ? '' : (':'+this._port))+(this._prefix)+SEEDLINK_PATH;
