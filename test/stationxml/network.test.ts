972b5a1edee9ee5049cecd240b3024ed69157ade
Philip Crotwell <crotwell@seis.sc.edu>
2022-02-15 11:44:16 -0500

    enumerate stations and channels tests

--- test/stationxml/network.test.ts
+++ test/stationxml/network.test.ts
@@ -1,9 +1,23 @@
 // @flow
 
-import {Network} from '../../src/stationxml';
+import {Network, allStations, allChannels} from '../../src/stationxml';
 
 test("simple network", () => {
   let NET_CODE = "AA";
   let net = new Network(NET_CODE);
   expect(net.networkCode).toBe(NET_CODE);
+});
+
+test("empty iterate", () => {
+  let inventory = new Array<Network>(0);
+  let i=0;
+  for (let s of allStations(inventory)) {
+    i+=1;
+  }
+  expect(i).toEqual(0);
+  let chanList = allChannels(inventory);
+  for (let c of chanList) {
+    i+=1;
+  }
+  expect(i).toEqual(0);
 });
