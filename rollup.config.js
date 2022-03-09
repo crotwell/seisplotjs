a2bf99dab6d1f4695c82b897992ca3062555d092
Philip Crotwell <crotwell@seis.sc.edu>
2022-03-09 15:05:26 -0500

    no more umd in rollup

--- rollup.config.js
+++ rollup.config.js
@@ -5,37 +5,6 @@ import ts from "rollup-plugin-ts";
 
 export default [{
   input: 'src/index.ts',
-  output: [
-    {
-      file: pkg.main,
-      name: "seisplotjs",
-      format: 'umd',
-      sourcemap: true,
-    }
-  ],
-  external: [
-    ...Object.keys(pkg.dependencies || {}),
-    ...Object.keys(pkg.peerDependencies || {}),
-    '@babel/runtime/helpers/classCallCheck',
-    '@babel/runtime/helpers/createClass',
-    '@babel/runtime/helpers/typeof',
-    '@babel/runtime/helpers/possibleConstructorReturn',
-    '@babel/runtime/helpers/getPrototypeOf',
-    '@babel/runtime/helpers/inherits',
-    '@babel/runtime/helpers/slicedToArray',
-    '@babel/runtime/helpers/wrapNativeSuper',
-    '@babel/runtime/regenerator',
-  ],
-  plugins: [
-    json(),
-    ts(),
-    babel({
-      exclude: 'node_modules/**',
-      babelHelpers: 'runtime',
-    })
-  ]
-},{
-  input: 'src/index.ts',
   //preserveModules: true,
   output: [
     {
