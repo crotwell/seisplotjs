0f17a8a1dd90bd82fa891c89d3b2bc76a2d68d9a
Philip Crotwell <crotwell@seis.sc.edu>
2022-03-23 10:57:47 -0400

    use html input type=date instead of pikaday

--- src/datechooser.ts
+++ src/datechooser.ts
@@ -3,7 +3,6 @@
  * University of South Carolina, 2019
  * http://www.seis.sc.edu
  */
-import Pikaday from "pikaday";
 import {DateTime, Duration} from "luxon";
 import {StartEndDuration, isoToDateTime} from "./util";
 
@@ -129,7 +128,7 @@ export class HourMinChooser extends HTMLElement {
         if (newTime !== mythis.time) {
           mythis.time = newTime;
         }
-        mythis.popupDiv.setAttribute("class", "hidden");
+        mythis.hide();
       } else {
         ntext.setAttribute("value", mythis.time.toFormat(HOUR_MIN_24));
       }
@@ -215,243 +214,6 @@ console.log(`hourmin popup position: {left: ${left} px; top: ${top} px;}`)
 customElements.define('hour-min-chooser', HourMinChooser);
 
 
-/**
- * CSS for the pikaday chooser.
- */
-export const pikaday_css = `
-@charset "UTF-8";
-
-/*!
- * Pikaday
- * Copyright © 2014 David Bushell | BSD & MIT license | http://dbushell.com/
- */
-
-.pika-single {
-    z-index: 9999;
-    display: block;
-    position: relative;
-    color: #333;
-    background: #fff;
-    border: 1px solid #ccc;
-    border-bottom-color: #bbb;
-    font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
-}
-
-/*
-clear child float (pika-lendar), using the famous micro clearfix hack
-http://nicolasgallagher.com/micro-clearfix-hack/
-*/
-.pika-single:before,
-.pika-single:after {
-    content: " ";
-    display: table;
-}
-.pika-single:after { clear: both }
-
-.pika-single.is-hidden {
-    display: none;
-}
-
-.pika-single.is-bound {
-    position: absolute;
-    box-shadow: 0 5px 15px -5px rgba(0,0,0,.5);
-}
-
-.pika-lendar {
-    float: left;
-    width: 240px;
-    margin: 8px;
-}
-
-.pika-title {
-    position: relative;
-    text-align: center;
-}
-
-.pika-label {
-    display: inline-block;
-    position: relative;
-    z-index: 9999;
-    overflow: hidden;
-    margin: 0;
-    padding: 5px 3px;
-    font-size: 14px;
-    line-height: 20px;
-    font-weight: bold;
-    background-color: #fff;
-}
-.pika-title select {
-    cursor: pointer;
-    position: absolute;
-    z-index: 9998;
-    margin: 0;
-    left: 0;
-    top: 5px;
-    opacity: 0;
-}
-
-.pika-prev,
-.pika-next {
-    display: block;
-    cursor: pointer;
-    position: relative;
-    outline: none;
-    border: 0;
-    padding: 0;
-    width: 20px;
-    height: 30px;
-    /* hide text using text-indent trick, using width value (it's enough) */
-    text-indent: 20px;
-    white-space: nowrap;
-    overflow: hidden;
-    background-color: transparent;
-    background-position: center center;
-    background-repeat: no-repeat;
-    background-size: 75% 75%;
-    opacity: .5;
-}
-
-.pika-prev:hover,
-.pika-next:hover {
-    opacity: 1;
-}
-
-.pika-prev,
-.is-rtl .pika-next {
-    float: left;
-    background-image: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAeCAYAAAAsEj5rAAAAUklEQVR42u3VMQoAIBADQf8Pgj+OD9hG2CtONJB2ymQkKe0HbwAP0xucDiQWARITIDEBEnMgMQ8S8+AqBIl6kKgHiXqQqAeJepBo/z38J/U0uAHlaBkBl9I4GwAAAABJRU5ErkJggg==');
-}
-
-.pika-next,
-.is-rtl .pika-prev {
-    float: right;
-    background-image: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAeCAYAAAAsEj5rAAAAU0lEQVR42u3VOwoAMAgE0dwfAnNjU26bYkBCFGwfiL9VVWoO+BJ4Gf3gtsEKKoFBNTCoCAYVwaAiGNQGMUHMkjGbgjk2mIONuXo0nC8XnCf1JXgArVIZAQh5TKYAAAAASUVORK5CYII=');
-}
-
-.pika-prev.is-disabled,
-.pika-next.is-disabled {
-    cursor: default;
-    opacity: .2;
-}
-
-.pika-select {
-    display: inline-block;
-}
-
-.pika-table {
-    width: 100%;
-    border-collapse: collapse;
-    border-spacing: 0;
-    border: 0;
-}
-
-.pika-table th,
-.pika-table td {
-    width: 14.285714285714286%;
-    padding: 0;
-}
-
-.pika-table th {
-    color: #999;
-    font-size: 12px;
-    line-height: 25px;
-    font-weight: bold;
-    text-align: center;
-}
-
-.pika-button {
-    cursor: pointer;
-    display: block;
-    box-sizing: border-box;
-    -moz-box-sizing: border-box;
-    outline: none;
-    border: 0;
-    margin: 0;
-    width: 100%;
-    padding: 5px;
-    color: #666;
-    font-size: 12px;
-    line-height: 15px;
-    text-align: right;
-    background: #f5f5f5;
-}
-
-.pika-week {
-    font-size: 11px;
-    color: #999;
-}
-
-.is-today .pika-button {
-    color: #33aaff;
-    font-weight: bold;
-}
-
-.is-selected .pika-button,
-.has-event .pika-button {
-    color: #fff;
-    font-weight: bold;
-    background: #33aaff;
-    box-shadow: inset 0 1px 3px #178fe5;
-    border-radius: 3px;
-}
-
-.has-event .pika-button {
-    background: #005da9;
-    box-shadow: inset 0 1px 3px #0076c9;
-}
-
-.is-disabled .pika-button,
-.is-inrange .pika-button {
-    background: #D5E9F7;
-}
-
-.is-startrange .pika-button {
-    color: #fff;
-    background: #6CB31D;
-    box-shadow: none;
-    border-radius: 3px;
-}
-
-.is-endrange .pika-button {
-    color: #fff;
-    background: #33aaff;
-    box-shadow: none;
-    border-radius: 3px;
-}
-
-.is-disabled .pika-button {
-    pointer-events: none;
-    cursor: default;
-    color: #999;
-    opacity: .3;
-}
-
-.is-outside-current-month .pika-button {
-    color: #999;
-    opacity: .3;
-}
-
-.is-selection-disabled {
-    pointer-events: none;
-    cursor: default;
-}
-
-.pika-button:hover,
-.pika-row.pick-whole-week:hover .pika-button {
-    color: #fff;
-    background: #ff8000;
-    box-shadow: none;
-    border-radius: 3px;
-}
-
-/* styling for abbr */
-.pika-table abbr {
-    border-bottom: none;
-    cursor: help;
-}
-
-`;
-
 
 /**
  * Date and Time chooser using pikaday for the date and the above
@@ -466,7 +228,6 @@ export class DateTimeChooser extends HTMLElement {
 
   _time: DateTime;
   updateCallback: (time: DateTime) => void;
-  picker: Pikaday;
   hourMin: HourMinChooser;
 
   constructor() {
@@ -483,23 +244,11 @@ export class DateTimeChooser extends HTMLElement {
     const shadow = this.attachShadow({mode: 'open'});
 
     const wrapper = document.createElement('span');
-    wrapper.addEventListener('mouseleave', e => {
-      console.log("mouseleave wrapper")
-      //mythis.picker.hide();
-    });
-    // style
-    let style = document.createElement('style');
-    style.textContent = pikaday_css + `
-          input {
-            width: 6em;
-          }
-        `;
-    shadow.appendChild(style);
 
     const dateField = wrapper.appendChild(document.createElement('input'));
-    dateField.setAttribute('type','text');
-    dateField.setAttribute('name','pikaday');
-    dateField.setAttribute('class','pikaday');
+    dateField.setAttribute('type','date');
+    dateField.setAttribute('name','date');
+    dateField.setAttribute('class','date');
     dateField.setAttribute('value',this._time.toISODate());
 
     const hourMin = wrapper.appendChild(document.createElement('hour-min-chooser')) as HourMinChooser;
@@ -509,40 +258,28 @@ export class DateTimeChooser extends HTMLElement {
     };
     hourMin._time = mythis.time;
     this.hourMin = hourMin;
+    dateField.onchange = function(e) {
+      const value = dateField.value;
+      console.log(`got change ${value}`);
 
-    dateField.onclick = function(e) {
-      console.log('ntext onclick -> showHide')
-      e.stopPropagation();
-      if (mythis.picker.isVisible()) {
-        mythis.picker.hide();
-      }
-    };
-    shadow.appendChild(wrapper);
-
-    this.picker = new Pikaday({
-      field: dateField,
-      container: wrapper,
-      //  trigger: inputField.node(),
-      format: "YYYY-MM-DD",
-      onSelect: function () {
-        let pikaValue = mythis.picker.getDate();
+        let pikaValue = DateTime.fromISO(value);
         let origTime = mythis._time;
         console.log(`pika: ${pikaValue}  prev: ${origTime}`)
         if (
           pikaValue && (
-            origTime.year !== pikaValue.getFullYear() ||
-            origTime.month !== pikaValue.getMonth() +1 ||
-            origTime.day !== pikaValue.getDate()
+            origTime.year !== pikaValue.year ||
+            origTime.month !== pikaValue.month ||
+            origTime.day !== pikaValue.day
           )
         ) {
-          mythis.time = mythis.time.set({ year: pikaValue.getFullYear(),
-                           month: pikaValue.getMonth() +1,
-                             day: pikaValue.getDate()
+          mythis.time = mythis.time.set({ year: pikaValue.year,
+                           month: pikaValue.month,
+                             day: pikaValue.day
                           });
           mythis.timeModified();
         }
-      },
-    });
+    }
+    shadow.appendChild(wrapper);
 
     this._internalSetTime(this.time);
   }
@@ -579,10 +316,8 @@ export class DateTimeChooser extends HTMLElement {
    */
   _internalSetTime(newTime: DateTime): void {
     this._time = newTime;
-    const ntext = (this.shadowRoot?.querySelector('input.pikaday') as HTMLInputElement);
+    const ntext = (this.shadowRoot?.querySelector('input.date') as HTMLInputElement);
     ntext.setAttribute('value',this.time.toISODate());
-    // re-do as string to avoid utc issue, using utc messes up picker, so pretend
-    this.picker.setDate(this.time.toISO(), true);
     this.hourMin._internalSetTime(newTime);
   }
 }
@@ -684,15 +419,6 @@ export class TimeRangeChooser extends HTMLElement {
   }
 }
 customElements.define('time-range-chooser', TimeRangeChooser);
-
-/**
- * Dummy callback function for cases where user doesn't need this. Mainly here
- * to keep eslint happy.
- *
- * @param   t new time/time range, not used
- */
-// eslint-disable-next-line no-unused-vars
-function dummyCallback(t: DateTime) {}
 
 /**
  * CSS for the parts of HourMin, DateTime and TimeRange choosers
