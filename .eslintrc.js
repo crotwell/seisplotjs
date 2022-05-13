261833a07749b9e48c41683c3db02844bd7f4058
Philip Crotwell <crotwell@seis.sc.edu>
2022-05-13 19:01:04 -0400

    new lint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,40 @@
 module.exports = {
-    "extends": [
-      "eslint:recommended",
-      "plugin:jest/recommended",
-      "plugin:jsdoc/recommended"
-    ],
-    "parser": "@babel/eslint-parser",
-    "plugins": [
-      "promise",
-      "jest",
-      "jsdoc"
-    ],
-    "env": {
-      "es6": true,
-      "browser": true,
-      "jest/globals": true
-    },
-    "parserOptions": {
-        "ecmaVersion": 6,
-        "sourceType": "module",
-    },
-    "rules": {
-      "semi": ["error", "always"],
-      "no-var": ["error"],
-      "no-console": [ "error"],
-      "no-unused-vars": [ "error"],
-      "eqeqeq": ["error", "always"],
-      "jsdoc/require-param-type": ["off"],
-      "jsdoc/require-returns-type": ["off"],
-      "jsdoc/no-types": ["error"],
-      "jsdoc/require-jsdoc": ["off", {"require":{"MethodDefinition":true}}]
-    }
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+    "promise",
+    "jest",
+    "jsdoc"
+  ],
+  "extends": [
+    "eslint:recommended",
+    'plugin:@typescript-eslint/recommended',
+    "plugin:jest/recommended",
+    "plugin:jsdoc/recommended",
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  "env": {
+    "es6": true,
+    "browser": true,
+    "jest/globals": true
+  },
+  "parserOptions": {
+      "ecmaVersion": 6,
+      "sourceType": "module",
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
+  },
+  "rules": {
+    "semi": ["error", "always"],
+    "no-var": ["error"],
+    "no-console": [ "error"],
+    "no-unused-vars": [ "error"],
+    "eqeqeq": ["error", "always"],
+    "jsdoc/require-param-type": ["off"],
+    "jsdoc/require-returns-type": ["off"],
+    "jsdoc/no-types": ["error"],
+    "jsdoc/require-jsdoc": ["off", {"require":{"MethodDefinition":true}}]
+  }
 };
