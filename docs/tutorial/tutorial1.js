295ab9a9057ccc2557890344aec51ffad7346ed8
Philip Crotwell <crotwell@seis.sc.edu>
2022-06-15 16:39:06 -0400

    update tutorial 1

--- docs/tutorial/tutorial1.js
+++ docs/tutorial/tutorial1.js
@@ -2,24 +2,19 @@
 
 
 import {seismogram, seismograph, seismographconfig, util} from './seisplotjs_3.0.0-alpha.0_standalone.mjs';
-const Seismogram = seismogram.Seismogram;
-const SeismogramDisplayData = seismogram.SeismogramDisplayData;
-const SeismographConfig = seismographconfig.SeismographConfig;
-const Seismograph = seismograph.Seismograph;
-const isoToDateTime = util.isoToDateTime;
 
 let dataArray = new Float32Array(1000).map(function(d, i) {
   return Math.sin(2*Math.PI*i/100) * 100;
 });
 let sampleRate = 20;
-let start = isoToDateTime('2019-07-04T05:46:23');
-let myseismogram = Seismogram.createFromContiguousData(dataArray, sampleRate, start);
-const seisData = [ SeismogramDisplayData.fromSeismogram(myseismogram) ];
+let start = util.isoToDateTime('2019-07-04T05:46:23');
+let myseismogram = seismogram.Seismogram.createFromContiguousData(dataArray, sampleRate, start);
+const seisData = [ seismogram.SeismogramDisplayData.fromSeismogram(myseismogram) ];
 // snip start draw
 const div = document.querySelector('div#sinewave');
-const seisConfig = new SeismographConfig();
+const seisConfig = new seismographconfig.SeismographConfig();
 seisConfig.title = "A sine wave!";
 seisConfig.margin.top = 25;
-const graph = new Seismograph(seisData, seisConfig);
+const graph = new seismograph.Seismograph(seisData, seisConfig);
 div.appendChild(graph);
 // snip end
