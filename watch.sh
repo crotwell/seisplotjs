ad3973ed6be1114dfa3ab31c20077f1c76f4c984
Philip Crotwell <crotwell@seis.sc.edu>
2022-10-26 16:30:49 -0400

    watch runs tsc and test

--- watch.sh
+++ watch.sh
@@ -1,11 +1,11 @@
 #!/bin/zsh
 
 # compile once to start
-npm run compile && ./copyToDocs.sh
+npm run compile && ./copyToDocs.sh && npm run tsc
 
 # recompile on file change within 1 seconds, except version.ts
 fswatch -o -l 1 --exclude version.ts --exclude handlebarsimport.ts src | while read stuff; do
   echo $stuff
-  npm run compile && npm run tsc
+  npm run compile && npm run tsc && npm run test 
   echo "OK"
 done
