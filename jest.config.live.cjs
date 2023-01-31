9e9367809d2d8b9857e7b1b3e77c56c6eb45fe31
Philip Crotwell <crotwell@seis.sc.edu>
2023-01-31 16:38:18 -0500

    livetest needs jest to work with d3 esm too

--- jest.config.live.cjs
+++ jest.config.live.cjs
@@ -1,3 +1,25 @@
+
+const esModules = [
+'d3-array',
+'d3-scale',
+'d3-axis',
+'d3-selection',
+'d3-color',
+'d3-shape',
+'d3-dispatch',
+'d3-time',
+'d3-drag',
+'d3-time-format',
+'d3-ease',
+'d3-timer',
+'d3-format',
+'d3-transition',
+'d3-interpolate',
+'d3-zoom',
+'d3-path',
+'internmap',
+ ].join('|');
+
 module.exports = {
   testMatch: ['<rootDir>/testremotes/**/*_livetest.ts'],
   testEnvironment: 'jsdom',
@@ -12,11 +34,12 @@ module.exports = {
     '^(\\.{1,2}/.*)\\.js$': '$1',
   },
   "transform": {
-    "^.+\\.tsx?$": [
-      "esbuild-jest",
-      {
-        sourcemap: true
-      }
+    "^.+\\.[jt]sx?$": [
+      "esbuild-jest"
     ]
-  }
+  },
+  "transformIgnorePatterns": [
+      `node_modules/(?!${esModules})`
+    ],
+
 };
