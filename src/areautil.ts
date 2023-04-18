1503e4b958b0522ddc656e4ca2fdb06293f2414c
Philip Crotwell <crotwell@seis.sc.edu>
2023-04-18 16:20:33 -0400

    fix java convertions

--- src/areautil.ts
+++ src/areautil.ts
@@ -1,14 +1,17 @@
 
-export function inArea(Location[] bounds, Location point): boolean {
-    float lonA, latA, lonB, latB;
-    int inside = 0;
-    for(int i = 0; i < bounds.length; i++) {
+export function inArea(bounds: Array<Location>, point: Location): boolean {
+    lonA: number;
+    latA: number;
+    lonB: number;
+    latB: number;
+    inside = 0;
+    for(i = 0; i < bounds.length; i++) {
         lonA = bounds[i].longitude - point.longitude;
         latA = bounds[i].latitude - point.latitude;
         lonB = bounds[(i + 1) % bounds.length].longitude - point.longitude;
         latB = bounds[(i + 1) % bounds.length].latitude - point.latitude;
-        int check = polygonPointCheck(lonA, latA, lonB, latB);
-        if(check == 4) {
+        check = polygonPointCheck(lonA, latA, lonB, latB);
+        if(check === 4) {
             return true;
         }
         inside += check;
@@ -16,14 +19,14 @@ export function inArea(Location[] bounds, Location point): boolean {
     return (inside != 0);
 }
 
-export function polygonPointCheck(float lonA,
-                                     float latA,
-                                     float lonB,
-                                     float latB): number {
+export function polygonPointCheck(lonA: number,
+                                  latA: number,
+                                  lonB: number,
+                                  latB: number): number {
     if(latA * latB > 0) {
         return 0;
     }
-    if((lonA * latB != lonB * latA) || (lonA * lonB > 0)) {
+    if((lonA * latB !== lonB * latA) || (lonA * lonB > 0)) {
         if(latA * latB < 0) {
             if(latA > 0) {
                 if(latA * lonB >= lonA * latB) {
@@ -36,8 +39,8 @@ export function polygonPointCheck(float lonA,
             }
             return 2;
         }
-        if(latB == 0) {
-            if(latA == 0) {
+        if(latB === 0) {
+            if(latA === 0) {
                 return 0;
             } else if(lonB > 0) {
                 return 0;
