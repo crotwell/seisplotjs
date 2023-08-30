3ac0ce98c657d4e829fe1365441856f9160a7709
Philip Crotwell <crotwell@seis.sc.edu>
2023-08-30 09:10:06 -0400

    watch prints date

--- watch.sh
+++ watch.sh
@@ -2,10 +2,13 @@
 
 # compile once to start
 npm run compile || npm run tsc
+date
+echo "OK"
 
 # recompile on file change within 1 seconds, except version.ts
 fswatch -o -l 1 --exclude version.ts --exclude handlebarsimport.ts src | while read stuff; do
   echo $stuff
-  (npm run compile || npm run tsc) && npm run test 
+  (npm run compile || npm run tsc) && npm run test
+  date
   echo "OK"
 done
