72c067a86b6689a3ecfb78ff36e135c503df7967
Philip Crotwell <crotwell@seis.sc.edu>
2024-02-15 10:50:21 -0500

    better language in case node<=20, default to en-US

--- src/textformat.ts
+++ src/textformat.ts
@@ -1,29 +1,31 @@
 
-export const latlonFormat = new Intl.NumberFormat(navigator.languages[0], {
+const lang = ( typeof navigator!== "undefined" && navigator?.language ) ? navigator?.language : "en-US";
+
+export const latlonFormat = new Intl.NumberFormat(lang, {
   style: "unit",
   unit: "degree",
   unitDisplay: "narrow",
   maximumFractionDigits: 2,
 });
 
-export const magFormat = new Intl.NumberFormat(navigator.languages[0], {
+export const magFormat = new Intl.NumberFormat(lang, {
   style: "decimal",
   maximumFractionDigits: 2,
 });
 
-export const depthNoUnitFormat = new Intl.NumberFormat(navigator.languages[0], {
+export const depthNoUnitFormat = new Intl.NumberFormat(lang, {
   style: "decimal",
   maximumFractionDigits: 2,
 });
 
-export const depthFormat = new Intl.NumberFormat(navigator.languages[0], {
+export const depthFormat = new Intl.NumberFormat(lang, {
   style: "unit",
   unit: "kilometer",
   unitDisplay: "narrow",
   maximumFractionDigits: 2,
   minimumFractionDigits: 2,
 });
-export const depthMeterFormat = new Intl.NumberFormat(navigator.languages[0], {
+export const depthMeterFormat = new Intl.NumberFormat(lang, {
   style: "unit",
   unit: "meter",
   unitDisplay: "narrow",
