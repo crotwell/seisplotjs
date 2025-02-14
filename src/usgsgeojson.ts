d164d690707b7a8eb2a7b29203f1a999ad7012a8
Prabha Acharya <pacharya@gps.caltech.edu>
2025-02-14 11:03:19 -0800

    Fix issue in parseFeatureAsQuake. Corrected values saved in quake publicId and eventId

--- src/usgsgeojson.ts
+++ src/usgsgeojson.ts
@@ -231,11 +231,12 @@ export function parseGeoJSON(geojson: USGSGeoJsonSummary): EventParameters {
  */
 export function parseFeatureAsQuake(feature: USGSGeoJsonFeature): Quake {
   const quake = new Quake();
-  quake.publicId = `quakeml:earthquake.usgs.gov/fdsnws/event/1/query?eventid={feature.id}`;
+  quake.publicId = `quakeml:earthquake.usgs.gov/fdsnws/event/1/query?eventid=${feature.id}`;
   const p = feature.properties;
   if (p == null) {
     throw new Error("Geojson missing properties");
   }
+  quake.eventId=feature.id;
   quake.descriptionList.push(new EventDescription(p.title));
   const origin = new Origin(
     DateTime.fromMillis(p.time),
