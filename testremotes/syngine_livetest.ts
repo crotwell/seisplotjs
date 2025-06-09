746e616f89992d0de0d78779a02f37b6c26aff3b
Philip Crotwell <crotwell@seis.sc.edu>
2025-06-09 10:13:37 -0400

    syngine testcase

---
+++ testremotes/syngine_livetest.ts
@@ -0,0 +1,23 @@
+
+import {IRIS_HOST} from '../src/fdsncommon';
+import {setDefaultFetch} from '../src/util';
+import fetch from 'cross-fetch';
+setDefaultFetch(fetch);
+
+import * as syngine from '../src/syngine';
+
+test("formURL", () => {
+  const query = new syngine.SyngineQuery();
+  query.format(syngine.FORMAT_MINISEED);
+  expect(query.originTime('2010-02-27T06:30:00.000')).toBe(query);
+  expect(query.sourceLatitude(-35.98)).toBe(query);
+  expect(query.sourceLongitude(-73)).toBe(query);
+  expect(query.sourceDepthInMeters(23200)).toBe(query);
+  expect(query.sourceMomentTensor([1.04e22,-0.039e22,-1e22,0.304e22,-1.52,-0.119e22])).toBe(query);
+  expect(query.receiverLatitude(47.6)).toBe(query);
+  expect(query.receiverLongitude(-122.3)).toBe(query);
+  expect(query.components('Z')).toBe(query);
+  return query.querySeismograms().then(sddList => {
+    expect(sddList.length).toBe(1);
+  });
+});
