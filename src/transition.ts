52cd37d080ad2e5181b5aac8ff5fbb49be48859d
Philip Crotwell <crotwell@seis.sc.edu>
2025-09-04 17:15:24 -0400

    jdocs

--- src/transition.ts
+++ src/transition.ts
@@ -1,5 +1,14 @@
 
-export function transition(drawFn: (x: number) => void, duration_millis: number) {
+/**
+ * Repeatedly calls the given function with increasing values from 0 to 1,
+ * by rescheduling animation frame after completion. Value is calculated from
+ * the elapsed time with 0 being the beginning time of the transition and
+ * 1 corresponding to the duration given.
+ * @param  drawFn  drawing function to call with the value
+ * @param  duration_millis  milliseconds for the total transition
+ */
+export function transition(drawFn: (x: number) => void,
+    duration_millis: number) {
   let startTimestamp: DOMHighResTimeStamp|null = null;
   const transFn = (timestamp: DOMHighResTimeStamp) => {
     if (startTimestamp == null) { startTimestamp = timestamp;}
